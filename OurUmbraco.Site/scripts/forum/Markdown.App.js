239a46c788d5f3e1b03b8bcd24d3430573080c1a
Mads Krohn <sniffdk@gmail.com>
2013-02-21 11:07:02 +0100

    Edge case html encoding fun
    Some of the old posts/comments contains <pre> blocks. We need to handle
    the content of these block as pure text.

--- OurUmbraco.Site/scripts/forum/Markdown.App.js
+++ OurUmbraco.Site/scripts/forum/Markdown.App.js
@@ -9,16 +9,41 @@ Markdown.App = (function ($) {
         };
 
     return {
+        htmlEncode: function (value) {
+            return $('<div/>').text(value).html();
+        },
+        htmlDecode: function (value) {
+            return $('<div/>').html(value).text();
+        },
         getConverter: function () {
             if (!converter) {
                 converter = Markdown.getSanitizingConverter();
-                converter.hooks.chain("postConversion", function (html) {
+                
+                converter.hooks.chain("preConversion", function (text) {
+                    // For (old) html content, we need to make sure that Markdown doesn't parse code blocks as html
+                    text = text.replace(/<pre([\s\S]*?)<\/pre>/gim, function (match, p1) {
+                        var subtext = p1.substring(1).replace(/</gim, "&lt;");
+                        return "<pre>" + subtext + "</pre>";
+                    });
+                    
+                    return text;
+                });
+
+                converter.hooks.chain("postConversion", function (html) {                    
                     html = $("<div>" + html + "</div>");
                     var pres = html.find("pre").addClass("prettyprint");
+                    
+                    pres.each(function() {
+                        var pre = $(this);
+                        if (pre.children("code").length === 0) {
+                            pre.wrapInner("<code/>");
+                        }
+                    });
+
                     if (pres.length > 0) {
                         setTimeout(function () {
                             prettyPrint();
-                        }, 2000);
+                        }, 1000);
                     }
 
                     return html.html();
