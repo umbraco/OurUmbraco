00c962ac6f13a721ceda5e8a873d95eb04d5f5eb
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-01-03 14:05:30 +0100

    Some styling changes for releases, added a extension method

---
+++ uRelease/Controllers/NodeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using umbraco.interfaces;
+
+namespace uRelease.Controllers
+{
+    public static class NodeExtensions
+    {
+        public static string GetPropertyValue(this INode release, string propertyAlias)
+        {
+            return release.GetProperty(propertyAlias) == null ? "" : release.GetProperty(propertyAlias).Value;
+        }
+    }
+}
