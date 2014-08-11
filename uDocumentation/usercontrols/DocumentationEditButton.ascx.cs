00fc21b1df74132ca0f7d023cdb3cd577e1c4331
Warren Buckley <warren@creativewebspecialist.co.uk>
2014-08-11 21:30:44 +0100

    Work in Progress - Formatting URLs but GitHub is insanely picky about case, need a way to ensure we have original casing

--- uDocumentation/usercontrols/DocumentationEditButton.ascx.cs
+++ uDocumentation/usercontrols/DocumentationEditButton.ascx.cs
@@ -12,7 +12,7 @@ namespace uDocumentation.usercontrols
     {
         private readonly Node _currentNode  = Node.GetCurrent();
         public bool ShowButton              = false;
-        public static string BaseUrl        = "https://github.com/umbraco/Umbraco4Docs/blob/master/Documentation/";
+        public static string BaseUrl        = "https://github.com/umbraco/Umbraco4Docs/blob/master";
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -26,22 +26,24 @@ namespace uDocumentation.usercontrols
             //We got this far so we can show button
             ShowButton = true;
 
-            
 
             //Get the documentation link to the md file to edit on GitHub
-            var docUrl = string.Empty;
-
-            //Get the MarkDown path key from the Context Keys
-            //This is the full url - C:/inetpub/wwwroot/OurUmbraco/OurUmbraco.Site/documentation/installation/index.md
-            docUrl = HttpContext.Current.Items[MarkdownLogic.MarkdownPathKey].ToString();
+            //The MarkDown path key from the Context Keys (not correct casing, so can not trim)
+            //This is the full url - C:/inetpub/wwwroot/OurUmbraco/OurUmbraco.Site/documentation/installation/install-umbraco-with-microsoft-webmatrix.md
+            var docUrl = HttpContext.Current.Items[MarkdownLogic.MarkdownPathKey].ToString();
 
             //Make sure file exists on disk
             if (File.Exists(docUrl))
             {
                 //Need to get NEW key as needs to be the original MD filename 
                 //from Github including .md & correct casing of file
+                var originalUrl = HttpContext.Current.Items["umbOriginalUrl"].ToString();
 
+                //Ensure beginning part of url is right case for GitHub URL
+                originalUrl = originalUrl.Replace("/documentation/", "/Documentation/");
 
+                //Append the .md file extension
+                docUrl = string.Format("{0}{1}", originalUrl, ".md");
 
             }
             else
