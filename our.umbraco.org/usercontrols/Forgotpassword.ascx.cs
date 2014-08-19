8b5b9a79eec9022b973902acdf271f1229ae2112
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-08-19 08:56:47 +0200

    Using SmtpClient instead of umbraco.library.SendMail to send forgot password mails

--- our.umbraco.org/usercontrols/Forgotpassword.ascx.cs
+++ our.umbraco.org/usercontrols/Forgotpassword.ascx.cs
@@ -1,33 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Mail;
 using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace our.usercontrols {
-    public partial class Forgotpassword : System.Web.UI.UserControl {
-       protected void Page_Load(object sender, EventArgs e) {
+namespace our.usercontrols
+{
+    public partial class Forgotpassword : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
         }
 
-        protected void sendPass(object sender, EventArgs e) {
+        protected void sendPass(object sender, EventArgs e)
+        {
             string em = tb_email.Text;
 
             umbraco.cms.businesslogic.member.Member m = umbraco.cms.businesslogic.member.Member.GetMemberFromEmail(em);
 
-            if (m != null) {
-                string pass = RandomString(8, true);
+            if (m != null)
+            {
+                var pass = RandomString(8, true);
                 m.Password = pass;
                 m.Save();
 
-                StringBuilder email = new StringBuilder();
-                email.Append("<p>Hi " + m.Text + "</p>");
-                email.Append("<p>This is your new password for your account on http://our.umbraco.org:</p>");
-                email.Append("<p><strong>" + pass  + "</strong></p>");
-                email.Append("<br/><br/><p>All the best<br/> <em>The email robot</em></p>");
+                var email = "<p>Hi " + m.Text + "</p>";
+                email = email + "<p>This is your new password for your account on http://our.umbraco.org:</p>";
+                email = email + "<p><strong>" + pass + "</strong></p>";
+                email = email + "<br/><br/><p>All the best<br/> <em>The email robot</em></p>";
 
-                umbraco.library.SendMail("robot@umbraco.org", m.Email, "Your password to our.umbraco.org", email.ToString(), true);
+                var mailMessage = new MailMessage
+                {
+                    Subject = string.Format("Your password to our.umbraco.org"),
+                    Body = email,
+                    IsBodyHtml = true
+                };
+
+                mailMessage.To.Add(new MailAddress(m.Email));
+
+                mailMessage.From = new MailAddress("robot@umbraco.org");
+
+                var smtpClient = new SmtpClient();
+                smtpClient.Send(mailMessage);
 
                 msg.Visible = true;
                 msg.Text = "<div class='notice'><p>A new password has been sent to the email address: <strong>" + em + "</strong></p><p>Go to the <a href='/member/login.aspx'>login page</a></div>";
@@ -48,5 +65,5 @@ namespace our.usercontrols {
             return builder.ToString();
         }
     }
-    
+
 }
