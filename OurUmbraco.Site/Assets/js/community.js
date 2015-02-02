818047004f2969be175a2cf1489542d2b852dc66
TimGeyssens <tg@umbraco.com>
2015-02-02 12:00:33 +0100

    Mark comment as solution funtionality

---
+++ OurUmbraco.Site/Assets/js/community.js
@@ -0,0 +1,24 @@
+﻿var community = function () {
+    return {
+        /* FORUM */
+        markCommentAsSolution: function (commentId, topicId) {
+            $.post("/umbraco/api/forum/MarkAsSolution/"+commentId + "?topicId=" + topicId);
+        }
+    };
+}();
+
+
+$(function () {
+
+    /*FORUM*/
+    $(".comment a.solved").click(function (e) {
+        e.preventDefault();
+        var data = $(this).data();
+        var id = parseInt(data.id);
+        var topicId = parseInt(data.topic);
+        community.markCommentAsSolution(id, topicId);
+        $(this).closest(".comment").addClass('solution');
+        $(".comment a.solved").remove();
+
+    });
+});
