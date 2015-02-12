61030fdddb2171a5dfd13be87a21e6db198d8852
TimGeyssens <tg@umbraco.com>
2015-02-12 15:57:36 +0100

    Begin work on profile bit

---
+++ our.umbraco.org/Models/ProfileViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace our.Models
+{
+    public class ProfileViewModel
+    {
+        public string Avatar { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string RepeatPassword { get; set; }
+
+        public string Company { get; set; }
+
+        public string Bio { get; set; }
+
+        public string TwitterAlias { get; set; }
+
+        public string Location { get; set; }
+
+
+    }
+}
