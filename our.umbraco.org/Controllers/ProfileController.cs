8471ce50d0bbcc87f576069c005b3119e8b67826
TimGeyssens <tg@umbraco.com>
2015-02-17 17:20:43 +0100

    Edit profile form bits

---
+++ our.umbraco.org/Controllers/ProfileController.cs
@@ -0,0 +1,63 @@
+﻿using our.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace our.Controllers
+{
+    public class ProfileController: SurfaceController
+    {
+        public ActionResult Render()
+        {
+            var ms = Services.MemberService;
+            var mem = ms.GetById(Members.GetCurrentMemberId());
+            var m = new ProfileModel();
+            m.Name = mem.Name;
+            m.Email = mem.Email;
+            m.Bio = mem.GetValue<string>("profileText");
+            m.Location = mem.GetValue<string>("location");
+            m.Company = mem.GetValue<string>("company");
+            m.TwitterAlias = mem.GetValue<string>("twitter");
+
+            return PartialView("Profile", m);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleSubmit(ProfileModel model)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            var ms = Services.MemberService;
+            var mem = ms.GetById(Members.GetCurrentMemberId());
+
+            if (mem.Email != model.Email && memberService.GetByEmail(model.Email) != null)
+            {
+                ModelState.AddModelError("", "A Member with that email already exists");
+                return CurrentUmbracoPage();
+
+            }
+            mem.Name = model.Name ;
+            mem.Email = model.Email;
+            mem.Username = model.Email;
+            mem.SetValue("profileText",model.Bio);
+            mem.SetValue("location",model.Location);
+            mem.SetValue("company",model.Company);
+            mem.SetValue("twitter",model.TwitterAlias);
+
+            ms.Save(mem);
+
+            if(model.Password != string.Empty && model.RepeatPassword != string.Empty && model.Password == model.RepeatPassword)
+                memberService.SavePassword(member, model.Password);       
+
+            TempData["success"] = true;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
