067c6933d56a1dc6ae0f541d1b961bb837ec2b69
TimGeyssens <tg@umbraco.com>
2015-02-17 10:23:41 +0100

    Adds notification service

---
+++ NotificationsWeb/Services/NotificationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+
+namespace NotificationsWeb.Services
+{
+    public class NotificationService: IDisposable
+    {
+        private DatabaseContext DatabaseContext;
+
+        public NotificationService()
+        {
+            init(ApplicationContext.Current.DatabaseContext);
+        }
+
+        public NotificationService(DatabaseContext dbContext)
+        {
+            init(dbContext);
+        }
+
+        private void init(DatabaseContext dbContext)
+        {
+            DatabaseContext = dbContext;
+        }
+
+        public static NotificationService Instance
+        {
+            get
+            {
+                return Singleton<NotificationService>.UniqueInstance;
+            }
+        }
+
+        public void Dispose()
+        {
+            DatabaseContext.DisposeIfDisposable();
+        }
+    }
+}
