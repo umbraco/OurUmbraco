45ddddb7c856ce98fc7a426fce7f40f99eaf1119
Shannon <sdeminick@gmail.com>
2015-02-23 23:44:00 +0100

    Updates to latest examine commits new search controllers and views , updates examine config.

---
+++ our.umbraco.org/Models/SearchResultModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Examine;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace our.Models
+{
+    public class SearchResultModel : PublishedContentWrapped
+    {
+        public ISearchResults SearchResults { get; private set; }
+        public double Totalmilliseconds { get; private set; }
+        public string SearchTerm { get; private set; }
+
+        public SearchResultModel(IPublishedContent content, ISearchResults searchResults, double totalmilliseconds, string searchTerm)
+            : base(content)
+        {
+            SearchResults = searchResults;
+            Totalmilliseconds = totalmilliseconds;
+            SearchTerm = searchTerm;
+        }
+    }
+}
--- our.umbraco.org/usercontrols/ExamineSearchResults.ascx.cs
+++ our.umbraco.org/usercontrols/ExamineSearchResults.ascx.cs
@@ -9,211 +9,10 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Examine;
 using Examine.Providers;
-using System.Data.SqlClient;
 using Examine.SearchCriteria;
 
 namespace our.usercontrols
 {
-    public static class SearchResultExtensions
-    {
-        public static string getTitle(this SearchResult result)
-        {
-            if (result["__IndexType"] == "content")
-                return HttpContext.Current.Server.HtmlEncode(result["nodeName"]);
-            else if (result["__IndexType"] == "documents" || result["__IndexType"] == "documentation")
-                return HttpContext.Current.Server.HtmlEncode(result["Title"]);
-            else
-                return string.Empty;
-
-        }
-
-        public static string getDate(this SearchResult result)
-        {
-            try
-            {
-                if (result["__IndexType"] == "content")
-                    return HttpContext.Current.Server.HtmlEncode(string.Format("Last update: {0}", GetFormattedDateTime(result["updateDate"], "MMMM dd, yyyy")));
-
-                if (result["__IndexType"] == "documents")
-                    return HttpContext.Current.Server.HtmlEncode(string.Format("Created: {0} / Last update: {1}", DateTime.Parse(result["Created"]).ToString("MMMM dd, yyyy HH:mm"), DateTime.Parse(result["Updated"]).ToString("MMMM dd, yyyy HH:mm")));
-
-                if (result["__IndexType"] == "documentation")
-                    return HttpContext.Current.Server.HtmlEncode(string.Format("Last update: {0}", GetFormattedDateTime(result["dateCreatedSearchAble"], "MMMM dd, yyyy")));
-            }
-            catch (FormatException ex)
-            {
-                // Catches "String was not recognized as a valid DateTime." errors
-                // TODO: Figure out why these errors occur..
-            }
-
-            return string.Empty;
-        }
-
-        public static string getDateSortable(this SearchResult result)
-        {
-            try
-            {
-                if (result["__IndexType"] == "content")
-                    return GetFormattedDateTime(result["updateDate"], "yyyy-MM-dd HH:mm");
-
-                if (result["__IndexType"] == "documents")
-                    return DateTime.Parse(result["Updated"]).ToString("yyyy-MM-dd HH:mm");
-
-                if (result["__IndexType"] == "documentation")
-                    return GetFormattedDateTime(result["dateCreatedSearchAble"], "yyyy-MM-dd HH:mm");
-            }
-            catch (FormatException ex) 
-            {
-                // Catches "String was not recognized as a valid DateTime." errors
-                // TODO: Figure out why these errors occur..
-            }
-            return string.Empty;
-        }
-
-        public static string GetFormattedDateTime(string date, string format)
-        {
-            try
-            {
-                var dateTime = new DateTime(int.Parse(date.Substring(0, 4)), int.Parse(date.Substring(4, 2)), int.Parse(date.Substring(6, 2)));
-                return dateTime.ToString(format);
-            }
-            catch
-            {
-                return string.Empty;
-            }
-        }
-
-        public static string fullURL(this SearchResult result)
-        {
-            if (result["__IndexType"] == "content")
-                return umbraco.library.NiceUrl(result.Id);
-
-            else if (result["__IndexType"] == "documentation")
-            {
-                return result["url"];
-            }
-            else if (result["__IndexType"] == "documents")
-                return "TODO";
-            //return uForum.Library.NiceTopicUrl(result.Id);
-            else
-                return string.Empty;
-        }
-
-        public static string generateBlurb(this SearchResult result, int noOfChars)
-        {
-            var text = string.Empty;
-
-            if (result["__IndexType"] == "content")
-            {
-                switch (result.Fields["nodeTypeAlias"])
-                {
-                    case "WikiPage":
-                        //Wiki uses bodyText field
-                        if (result.Fields.ContainsKey("bodyText"))
-                            text = result.Fields["bodyText"];
-                        break;
-                    case "Project":
-                        //Project uses description field
-                        if (result.Fields.ContainsKey("descripiton"))
-                            text = result.Fields["description"];
-                        break;
-                }
-
-            }
-            else if (result["__IndexType"] == "documents")
-            {
-
-                try
-                {
-                    if (result.Fields.ContainsKey("Body"))
-                        text = result["Body"];
-                }
-                catch { }
-            }
-            else if (result["__IndexType"] == "documentation")
-            {
-
-                try
-                {
-                    if (result.Fields.ContainsKey("Body"))
-                        text = result["Body"];
-                }
-                catch { }
-            }
-
-            text = umbraco.library.StripHtml(text);
-
-            if (text.Length > noOfChars && text.Length > 0)
-            {
-                text = text.Substring(0, noOfChars) + "...";
-            }
-
-            return text;
-
-        }
-
-        public static string cssClassName(this SearchResult result)
-        {
-            string cssClass = string.Empty;
-
-            if (result["__IndexType"] == "content")
-            {
-                switch (result.Fields["nodeTypeAlias"])
-                {
-                    case "WikiPage":
-                        cssClass = " wiki ";
-                        break;
-                    case "Project":
-                        cssClass = " project ";
-                        break;
-                }
-            }
-            else if (result["__IndexType"] == "documentation")
-            {
-                cssClass = " documentation ";
-            }
-            else if (result["__IndexType"] == "documents")
-            {
-                cssClass = " forum ";
-
-
-                SqlConnection cn = new SqlConnection(umbraco.GlobalSettings.DbDSN);
-                cn.Open();
-                SqlCommand cmd = new SqlCommand("Select answer from forumTopics where id = @id", cn);
-                cmd.Parameters.AddWithValue("@id", result.Id);
-                int solved = 0;
-                try
-                {
-                    int.TryParse(cmd.ExecuteScalar().ToString(), out solved);
-                }
-                catch { }
-                cn.Close();
-
-                if (solved != 0)
-                    cssClass += "solution";
-            }
-
-
-            return cssClass;
-
-        }
-
-        public static string BuildExamineString(this string term, int boost, string field, bool andSearch)
-        {
-            term = Lucene.Net.QueryParsers.QueryParser.Escape(term);
-            var terms = term.Trim().Split(' ');
-            var qs = field + ":";
-            qs += "\"" + term + "\"^" + (boost + 30000).ToString() + " ";
-            qs += field + ":(+" + term.Replace(" ", " +") + ")^" + (boost + 5).ToString() + " ";
-            if (!andSearch)
-            {
-                qs += field + ":(" + term + ")^" + boost.ToString() + " ";
-            } return qs;
-        }
-
-
-    }
-
     public partial class ExamineSearchResults : System.Web.UI.UserControl
     {
 
@@ -305,12 +104,12 @@ namespace our.usercontrols
                 if (sortOrder != null && sortOrder.ToLower() == "desc")
                 {
                     OrderByDateDescLink.CssClass = OrderByDateDescLink.CssClass + "linkNoColor";
-                    searchResults = searchResults.OrderByDescending(x => x.getDateSortable());
+                    searchResults = searchResults.OrderByDescending(x => x.GetDateSortable());
                 }
                 else
                 {
                     OrderByDateAscLink.CssClass = OrderByDateAscLink.CssClass + "linkNoColor";
-                    searchResults = searchResults.OrderBy(x => x.getDateSortable());
+                    searchResults = searchResults.OrderBy(x => x.GetDateSortable());
                 }
             }
             else
