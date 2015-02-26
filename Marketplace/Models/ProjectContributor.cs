7cb60b28dc69110ea543e5e01b347ba71d7377f9
TimGeyssens <tg@umbraco.com>
2015-02-26 10:57:07 +0100

    Adds project contribution model/service/controller

---
+++ Marketplace/Models/ProjectContributor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Persistence;
+
+namespace Marketplace.Models
+{
+    [TableName("projectContributors")]
+    public class ProjectContributor
+    {
+        [Column("projectId")]
+        public int ProjectId { get; set; }
+
+        [Column("memberId")]
+        public int MemberId { get; set; }
+    }
+}
---
+++ Marketplace/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace
+{
+    public class Singleton<T> where T : class, new()
+    {
+        Singleton() { }
+
+        private static readonly Lazy<T> instance = new Lazy<T>(() => new T());
+
+        public static T UniqueInstance
+        {
+            get { return instance.Value; }
+        }
+    }
+}
