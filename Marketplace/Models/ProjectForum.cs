f065201529d57b99025d2b12707df2360973d11d
TimGeyssens <tg@umbraco.com>
2015-03-02 13:58:11 +0100

    Project forums crud working

--- Marketplace/Models/ProjectForum.cs
+++ Marketplace/Models/ProjectForum.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace Marketplace.Models
 {
+    [DataContract(Name = "projectForum", Namespace = "")]
     public class ProjectForum
     {
-        [Required]
+      
         public int Id { get; set; }
 
+        [DataMember(Name = "parentId",IsRequired=true)]
         [Required]
         public int ParentId { get; set; }
 
+        [DataMember(Name = "title", IsRequired = true)]
         [Required]
         public string Title { get; set; }
 
+        [DataMember(Name = "description", IsRequired = true)]
         [Required]
         public string Description { get; set; }
     }
