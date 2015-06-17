5c01eb8e03cec2319947923e39e03f1f6375d74a
Sebastiaan Janssen <sebastiaan@umbraco.com>
2015-06-17 22:52:53 +0200

    OUR-173 Current page is lost upon login

--- our.umbraco.org/usercontrols/Login.ascx.cs
+++ our.umbraco.org/usercontrols/Login.ascx.cs
@@ -5,33 +5,43 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace our.usercontrols {
-    public partial class Login : System.Web.UI.UserControl {
+namespace our.usercontrols
+{
+    public partial class Login : System.Web.UI.UserControl
+    {
         public int NextPage { get; set; }
         public string ErrorMessage { get; set; }
 
-        protected void Page_Load(object sender, EventArgs e) {
+        protected void Page_Load(object sender, EventArgs e)
+        {
             umbraco.library.RegisterJavaScriptFile("jquery.validation", "/scripts/jquery.validation.js");
         }
 
-        protected void login(object sender, EventArgs e) {
+        protected void login(object sender, EventArgs e)
+        {
             string email = tb_email.Text;
             string password = tb_password.Text;
             string redirectUrl = Request.QueryString["redirectUrl"];
 
             umbraco.cms.businesslogic.member.Member m = umbraco.cms.businesslogic.member.Member.GetMemberFromLoginNameAndPassword(email, password);
 
-            if (m != null) {
+            if (m != null)
+            {
                 umbraco.cms.businesslogic.member.Member.AddMemberToCache(m, false, new TimeSpan(30, 0, 0, 0));
 
-                
                 if (!string.IsNullOrEmpty(redirectUrl))
-                    Response.Redirect(redirectUrl);
+                {
+                    var nextUrl = new Uri(redirectUrl);
+                    if (Request.Url.Host == nextUrl.Host)
+                        Response.Redirect(redirectUrl);
+                }
 
                 if (NextPage > 0)
                     Response.Redirect(umbraco.library.NiceUrl(NextPage));
 
-            } else {
+            }
+            else
+            {
                 lt_err.Text = "<div class='error'><p>" + ErrorMessage + "</p></div>";
                 lt_err.Visible = true;
             }
