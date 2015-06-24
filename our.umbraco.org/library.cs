97a582773d0222b757938c95654f17a5efe8ba58
Sebastiaan Janssen <sebastiaan@umbraco.com>
2015-06-24 15:11:34 +0200

    HtmlEncode avatar url's to avoid spaces and other weird characters breaking it

--- our.umbraco.org/library.cs
+++ our.umbraco.org/library.cs
@@ -201,7 +201,7 @@ namespace our
         {
             var memberAvatarPath = MemberAvatarPath(member);
             if (string.IsNullOrWhiteSpace(memberAvatarPath) == false)
-                return GetLocalAvatar(member.GetPropertyValue("avatar").ToString(), avatarSize, member.Name);
+                return GetLocalAvatar(HttpUtility.HtmlEncode(member.GetPropertyValue("avatar").ToString()), avatarSize, member.Name);
 
             return GetGravatar(member.GetPropertyValue("Email").ToString(), avatarSize, member.Name);
         }
