9b2d9208ddf4854044f4a24c0bc726788f0089af
Sebastiaan Janssen <sebastiaan@umbraco.com>
2015-08-23 12:59:39 +0200

    OUR-324 confirming a search in the "Umbraco Package Repository" in backend with "enter" key loads wrong page.
    #OUR-324 Fixed

--- OurUmbraco.Site/scripts/repo/repoSearch.js
+++ OurUmbraco.Site/scripts/repo/repoSearch.js
@@ -1,50 +1,56 @@
+$(document).ready(function () {
 
-          
-      $(document).ready(function() {
-        
-        $("#search-field").focus(function() {
-          if ($("#search-field").val() == "Search...") {
+    $("#search-field").focus(function () {
+        if ($("#search-field").val() === "Search...") {
             $("#search-field").val("");
-          }
-        });
+        }
+    });
 
-        $("#search-field").blur(function() {
-          if ($("#search-field").val() == "") {
+    $("#search-field").blur(function () {
+        if ($("#search-field").val() === "") {
             $("#search-field").val("Search...");
-          }
-        });       
-        
-      });
-  $(function() {
-    $("#search-field").typeWatch({ highlight: true, wait: 200, captureLength: -1, callback: function() {
-      var query = $("#search-field").val();
-      if (query.length > 1) {
-        $("#search-field").addClass("search-loading");
-        // get data
-        $.getJSON("/umbraco/api/Search/FindProjects/?query=" + query + "&parent=0&wildcard=true", function(data){
-          // toggle UI
-          if (data.length > 0) {
-            $("#search-no-results").hide();            
-            $("#repo-content").hide();
-            $("#search-result-holder").empty();
-            $("#search-result-holder").show();
-        
-            // apply the jquery templates
-            $( "#search-result" ).tmpl( data).appendTo( "#search-result-holder"); 
-          } else {
-            $("#search-result-holder").hide();
-            $("#repo-content").show();
-            $("#search-query").text(query);
-            $("#search-no-results").fadeIn('slow');            
-          }
+        }
+    });
+    
+    $("#search-field").bind("keypress", function (e) {
+        if (e.keyCode === 13) {
+            return false;
+        }
+        return true;
+    });
+});
 
-          $("#search-field").removeClass("search-loading");
-        });
-        
-      } else {
-        $("#search-no-results").hide();            
-        $("#search-result-holder").hide();
-        $("#repo-content").show();
-      }
-    }});
-  });
+$(function () {
+    $("#search-field").typeWatch({
+        highlight: true, wait: 200, captureLength: -1, callback: function () {
+            var query = $("#search-field").val();
+            if (query.length > 1) {
+                $("#search-field").addClass("search-loading");
+                // get data
+                $.getJSON("/umbraco/api/Search/FindProjects/?query=" + query + "&parent=0&wildcard=true", function (data) {
+                    // toggle UI
+                    if (data.length > 0) {
+                        $("#search-no-results").hide();
+                        $("#repo-content").hide();
+                        $("#search-result-holder").empty();
+                        $("#search-result-holder").show();
+
+                        // apply the jquery templates
+                        $("#search-result").tmpl(data).appendTo("#search-result-holder");
+                    } else {
+                        $("#search-result-holder").hide();
+                        $("#repo-content").show();
+                        $("#search-query").text(query);
+                        $("#search-no-results").fadeIn('slow');
+                    }
+
+                    $("#search-field").removeClass("search-loading");
+                });
+            } else {
+                $("#search-no-results").hide();
+                $("#search-result-holder").hide();
+                $("#repo-content").show();
+            }
+        }
+    });
+});
