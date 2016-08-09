85ed5b5c666a99ff98d71d446cc31730fa9aae99
Shannon <sdeminick@gmail.com>
2016-08-10 01:18:23 +0200

    reverts KarmaProvider, it's legacy, not sure if it's actually used (hopefully not!)

--- OurUmbraco/MarketPlace/Providers/KarmaProvider.cs
+++ OurUmbraco/MarketPlace/Providers/KarmaProvider.cs
@@ -2,8 +2,6 @@
 using System.Linq;
 using OurUmbraco.MarketPlace.Interfaces;
 using OurUmbraco.Our;
-using Umbraco.Core;
-using Umbraco.Core.Persistence;
 using Umbraco.Web;
 
 namespace OurUmbraco.MarketPlace.Providers
@@ -19,28 +17,9 @@ namespace OurUmbraco.MarketPlace.Providers
 
         public IEnumerable<IKarma> GetProjectsKarmaList()
         {
-            return GetProjectsKarmaList(null);
-        }
-
-        public IEnumerable<IKarma> GetProjectsKarmaList(int[] ids)
-        {
             var karmaList = new List<IKarma>();
 
-            string sql;
-            if (ids == null || ids.Length == 0)
-            {
-                sql = "SELECT id as ProjectId, SUM(points) Points FROM powersProject GROUP BY id ORDER BY SUM(points) DESC";
-            }
-            else
-            {
-                if (ids.Length > 2000) throw new InvalidOperationException("Cannot query for more than 2000 items");
-
-                sql = string.Format(
-                    "SELECT id as ProjectId, SUM(points) Points FROM powersProject WHERE id IN ({0}) GROUP BY id ORDER BY SUM(points) DESC",
-                    string.Join(",", ids));
-            }
-                
-            using (var reader = Data.SqlHelper.ExecuteReader(sql))
+            using (var reader = Data.SqlHelper.ExecuteReader("SELECT id as ProjectId, SUM(points) Points FROM powersProject GROUP BY id ORDER BY SUM(points) DESC"))
             {
                 while (reader.Read())
                 {
@@ -54,20 +33,6 @@ namespace OurUmbraco.MarketPlace.Providers
             }
 
             return karmaList;
-        }
-
-        public IEnumerable<IKarma> GetProjectsKarmaList(long pageIndex, long pageSize, out long totalItems)
-        {
-            var pagedResult = ApplicationContext.Current.DatabaseContext.Database.Page<dynamic>(pageIndex + 1, pageSize,
-                "SELECT * FROM (SELECT id as ProjectId, SUM(points) Points FROM powersProject GROUP BY id) tmp ORDER BY Points DESC");
-
-            totalItems = pagedResult.TotalItems;
-
-            return pagedResult.Items.Select(x => new Karma
-            {
-                ProjectId = x.ProjectId,
-                Points = x.Points
-            });
         }
 
         public IEnumerable<IKarma> GetProjectsKarmaListByDate(DateTime afterDate)
