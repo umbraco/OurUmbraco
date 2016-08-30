3e661e6cdd12471bccedb8aa3ae059a95177c062
Sebastiaan Janssen <sebastiaan@umbraco.com>
2016-08-30 20:24:43 +0200

    If the link isn't to Our Umbraco, make it open in a new tab and add rel=nofollow

--- OurUmbraco/Project/ProjectExtensions.cs
+++ OurUmbraco/Project/ProjectExtensions.cs
@@ -138,6 +138,13 @@ namespace OurUmbraco.Project
                     if (href != null)
                     {
                         element.Attributes.Add("href", href.Value);
+                        if (href.Value.StartsWith("https://our.umbraco.org/") == false &&
+                            href.Value.StartsWith("http://our.umbraco.org/") == false &&
+                            href.Value.StartsWith("our.umbraco.org/") == false)
+                        {
+                            element.Attributes.Add("target", "_blank");
+                            element.Attributes.Add("rel", "nofollow");
+                        }
                     }
 
                     if (src != null)
