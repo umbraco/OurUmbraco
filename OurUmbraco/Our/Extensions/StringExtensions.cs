92babb86748539f5602a092f9418de2e628f7039
Sebastiaan Janssen <sebastiaan@umbraco.com>
2016-09-06 13:13:29 +0200

    OUR-395 Search terms with square brackets YSOD

---
+++ OurUmbraco/Our/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OurUmbraco.Our.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// taken from https://our.umbraco.org/projects/website-utilities/ezsearch/bugs-feedback-suggestions/59447-Special-Characters-in-Search-Term#comment-215504
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static string MakeSearchQuerySafe(this string query)
+        {
+            var regex = new Regex(@"[^\w\s-]");
+            return regex.Replace(query, "");
+        }
+    }
+}
