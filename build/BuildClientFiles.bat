166007c66859bb78f1c4788abce614f156947523
Sebastiaan Janssen <sebastiaan@umbraco.com>
2016-09-18 12:12:52 +0200

    Use node 4.5 for building

--- build/BuildClientFiles.bat
+++ build/BuildClientFiles.bat
@@ -3,21 +3,31 @@ SETLOCAL
 	:: SETLOCAL is on, so changes to the path not persist to the actual user's path
 	
 SET release=%1
-ECHO Installing Npm NuGet Package
-
-SET nuGetFolder=%CD%\..\packages\
-ECHO Configured packages folder: %nuGetFolder%
+SET toolsFolder=%CD%\tools\
 ECHO Current folder: %CD%
 
-%CD%\..\.nuget\NuGet.exe install Npm.js -OutputDirectory %nuGetFolder%  -Verbosity quiet
+SET nodeFileName=node-v4.5.0-win-x86.7z
+SET nodeExtractFolder=%toolsFolder%node.js.450
+IF NOT EXIST %nodeExtractFolder% (
+	ECHO Downloading node.js
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('http://nodejs.org/dist/v4.5.0/%nodeFileName%', '%toolsFolder%\%nodeFileName%')"
+	ECHO Extracting node.js
+	%toolsFolder%\7za.exe x %toolsFolder%\%nodeFileName% -o%nodeExtractFolder% -aos > nul
+)
+FOR /f "delims=" %%A in ('dir %nodeExtractFolder%\node* /b') DO SET "nodePath=%nodeExtractFolder%\%%A"
 
-for /f "delims=" %%A in ('dir %nuGetFolder%node.js.* /b') do set "nodePath=%nuGetFolder%%%A\"
-for /f "delims=" %%A in ('dir %nuGetFolder%npm.js.* /b') do set "npmPath=%nuGetFolder%%%A\tools\"
+SET nuGetFolder=%CD%\..\packages\
+FOR /f "delims=" %%A in ('dir %nuGetFolder%npm.js.* /b') DO SET "npmPath=%nuGetFolder%%%A\tools\"
+IF %npmPath% == [] (
+	ECHO Downloading npm
+	ECHO Configured packages folder: %nuGetFolder%	
+	ECHO Installing Npm NuGet Package
+	%CD%\..\.nuget\NuGet.exe install Npm.js -OutputDirectory %nuGetFolder%  -Verbosity quiet
+	FOR /f "delims=" %%A in ('dir %nuGetFolder%npm.js.* /b') DO SET "npmPath=%nuGetFolder%%%A\tools\"
+)
 
 REM Ensures that we look for the just downloaded NPM, not whatever the user has installed on their machine
-path=%npmPath%;%nodePath%
-
-ECHO %path%
+PATH=%npmPath%;%nodePath%
 
 SET buildFolder=%CD%
 
@@ -25,10 +35,11 @@ ECHO Change directory to %CD%\..\OurUmbraco.Client\
 CD %CD%\..\OurUmbraco.Client\
 
 ECHO Do npm install and the gulp build
-call npm cache clean
-call npm install
-call npm install -g install gulp -g --quiet
-call gulp build
+SET npm="%nodePath%\node.exe" "%npmPath%node_modules\npm\bin\npm-cli.js" %*
+%npm% cache clean
+%npm% install
+%npm% install -g install gulp -g --quiet
+gulp build
 
 ECHO Move back to the build folder
 CD %buildFolder% 
--- build/RunGulp.bat
+++ build/RunGulp.bat
@@ -1,23 +1,29 @@
 @ECHO OFF
 SETLOCAL
 	:: SETLOCAL is on, so changes to the path not persist to the actual user's path
-
+	
 SET release=%1
-ECHO Installing Npm NuGet Package
+SET toolsFolder=%CD%\tools\
+ECHO Current folder: %CD%
+
+SET nodeFileName=node-v4.5.0-win-x86.7z
+SET nodeExtractFolder=%toolsFolder%node.js.450
+IF NOT EXIST %nodeExtractFolder% (
+	ECHO Downloading node.js
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('http://nodejs.org/dist/v4.5.0/%nodeFileName%', '%toolsFolder%\%nodeFileName%')"
+	ECHO Extracting node.js
+	%toolsFolder%\7za.exe x %toolsFolder%\%nodeFileName% -o%nodeExtractFolder% -aos > nul
+)
+FOR /f "delims=" %%A in ('dir %nodeExtractFolder%\node* /b') DO SET "nodePath=%nodeExtractFolder%\%%A"
 
 SET nuGetFolder=%CD%\..\packages\
 ECHO Configured packages folder: %nuGetFolder%
-ECHO Current folder: %CD%
-
+ECHO Installing Npm NuGet Package
 %CD%\..\.nuget\NuGet.exe install Npm.js -OutputDirectory %nuGetFolder%  -Verbosity quiet
-
-for /f "delims=" %%A in ('dir %nuGetFolder%node.js.* /b') do set "nodePath=%nuGetFolder%%%A\"
-for /f "delims=" %%A in ('dir %nuGetFolder%npm.js.* /b') do set "npmPath=%nuGetFolder%%%A\tools\"
+FOR /f "delims=" %%A in ('dir %nuGetFolder%npm.js.* /b') DO SET "npmPath=%nuGetFolder%%%A\tools\"
 
 REM Ensures that we look for the just downloaded NPM, not whatever the user has installed on their machine
-path=%npmPath%;%nodePath%
-
-ECHO %path%
+PATH=%npmPath%;%nodePath%
 
 SET buildFolder=%CD%
 
@@ -25,9 +31,11 @@ ECHO Change directory to %CD%\..\OurUmbraco.Client\
 CD %CD%\..\OurUmbraco.Client\
 
 ECHO Do npm install and the gulp build
-call npm install
-call npm install -g install gulp -g --quiet
-call gulp
+SET npm="%nodePath%\node.exe" "%npmPath%node_modules\npm\bin\npm-cli.js" %*
+%npm% cache clean
+%npm% install
+%npm% install -g install gulp -g --quiet
+gulp
 
 ECHO Move back to the build folder
 CD %buildFolder% 
