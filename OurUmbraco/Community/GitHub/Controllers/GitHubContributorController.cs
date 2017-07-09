e2806e902fa33db4bc60ff444a33e15234692d30
Anders Bjerner <abjerner@skybrud.dk>
2017-07-09 23:38:28 +0200

    GitHub contributors: Cleaned up and rewrote the controller
    The controller is now much simpler as all the logic has been moved to the service class.

--- OurUmbraco/Community/GitHub/Controllers/GitHubContributorController.cs
+++ OurUmbraco/Community/GitHub/Controllers/GitHubContributorController.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Text;
 using System.Web.Mvc;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using OurUmbraco.Community.GitHub.Models;
 using OurUmbraco.Community.GitHub.Models.Cached;
-using RestSharp;
 using Umbraco.Core.Logging;
 using Umbraco.Web.Mvc;
 
@@ -16,43 +11,6 @@ namespace OurUmbraco.Community.GitHub.Controllers
 {
     public class GitHubContributorController : SurfaceController
     {
-        /// <summary>
-        /// Repositories to include in the combination of contributions
-        /// </summary>
-        private readonly string[] Repositories =
-        {
-            "Umbraco-CMS",
-            "UmbracoDocs",
-            "OurUmbraco",
-            "Umbraco.Deploy.Contrib",
-            "Umbraco.Courier.Contrib",
-            "Umbraco.Deploy.ValueConnectors"
-        };
-
-        protected string JsonPath
-        {
-            get { return Server.MapPath("~/App_Data/TEMP/GithubContributors.json"); }
-        }
-
-        public List<GitHubCachedGlobalContributorModel> GetCachedContributors()
-        {
-
-            if (!System.IO.File.Exists(JsonPath)) throw new Exception("The JSON file doesn't exist on disk");
-
-
-            List<GitHubCachedGlobalContributorModel> temp = new List<GitHubCachedGlobalContributorModel>();
-
-            // Parse the JSON file from disk
-            foreach (JToken token in Skybrud.Essentials.Json.JsonUtils.LoadJsonArray(JsonPath))
-            {
-
-                temp.Add(token.ToObject<GitHubCachedGlobalContributorModel>());
-
-            }
-
-            return temp;
-
-        }
 
         /// <summary>
         /// Gets data for all GitHub contributors for all listed Umbraco repositories, 
@@ -64,109 +22,55 @@ namespace OurUmbraco.Community.GitHub.Controllers
             var model = new GitHubContributorsModel();
             model.Contributors = new List<GitHubCachedGlobalContributorModel>();
 
-            try
-            {
+            GitHubService service = new GitHubService();
 
-                // If the file exists on disk and hasn't expired (AKA not older than one day), we should just load that
-                if (force == false && System.IO.File.Exists(JsonPath) && System.IO.File.GetLastWriteTimeUtc(JsonPath) >= DateTime.UtcNow.AddDays(-1))
-                {
-                    model.Contributors = GetCachedContributors();
-                    return PartialView("~/Views/Partials/Home/GitHubContributors.cshtml", model);
-                }
+
+            if (force)
+            {
 
                 try
                 {
-                    // Load the contributors via the GitHub API and map to the cached model
-                    var contributors = GetContributors().Select(x => new GitHubCachedGlobalContributorModel(x));
 
-                    // Serialize the contributors to raw JSON
-                    string rawJson = JsonConvert.SerializeObject(contributors, Formatting.Indented);
+                    // Update the cached contributors
+                    GitHubContributorsResult result = service.UpdateOverallContributors();
 
-                    // Save the JSON to disk
-                    System.IO.File.WriteAllText(JsonPath, rawJson, Encoding.UTF8);
+                    // Update the model with the contributors
+                    model.Contributors = result.Contributors.Select(x => new GitHubCachedGlobalContributorModel(x)).ToList();
 
-                    model.Contributors = contributors.ToList();
                 }
                 catch (Exception ex)
                 {
-                    // Log the error so we can debug it later
-                    LogHelper.Error<GitHubContributorController>("Unable to load GitHub contributors from the GitHub API", ex);
 
-                    // Load the contributors from the cache (if we have any)
-                    model.Contributors = GetCachedContributors();
+                    // Log the error so we can debug it later
+                    LogHelper.Error<GitHubContributorController>("Unable to load GitHub contributors from the GitHub", ex);
+
                 }
 
             }
-            catch (Exception ex)
+            else if (System.IO.File.Exists(service.JsonPath))
             {
-                // Log the error so we can debug it later
-                LogHelper.Error<GitHubContributorController>("Unable to load GitHub contributors from the GitHub API", ex);
+
+                try
+                {
+
+                    // Update the model with the contributors
+                    model.Contributors = service.GetOverallContributorsFromDisk();
+
+                }
+                catch (Exception ex)
+                {
+                    
+                    // Log the error so we can debug it later
+                    LogHelper.Error<GitHubContributorController>("Unable to load GitHub contributors from the GitHub API or cache", ex);
+
+                }
 
             }
 
             return PartialView("~/Views/Partials/Home/GitHubContributors.cshtml", model);
-        }
-
-        public List<GitHubGlobalContributorModel> GetContributors()
-        {
-
-
-            string configPath = Server.MapPath("~/config/githubhq.txt");
-            if (!System.IO.File.Exists(configPath))
-            {
-                LogHelper.Debug<GitHubContributorController>("Config file was not found: " + configPath);
-                throw new Exception("Config file was not found: " + configPath);
-            }
-
-            string[] login = System.IO.File.ReadAllLines(configPath).Where(x => x.Trim() != "").Distinct().ToArray();
-
-            var githubController = new GitHubService();
-            var gitHubContributors = new List<GitHubContributorModel>();
-            foreach (var repo in Repositories)
-            {
-                var response = githubController.GetAllRepoContributors(repo);
-                if (response.StatusCode == HttpStatusCode.OK &&
-                    response.ResponseStatus == ResponseStatus.Completed)
-                {
-                    gitHubContributors.AddRange(response.Data);
-                }
-                else
-                {
-                    LogHelper.Warn<IGitHubContributorsModel>(string.Format("Invalid HTTP response for repository {0}", repo));
-                }
-            }
-
-            // filter to only include items from the last year (if we ran 4.6.1 we could have used ToUnixTimeSeconds())
-            var filteredRange = DateTime.UtcNow.AddYears(-1).Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
-
-            foreach (var contrib in gitHubContributors)
-            {
-                var contribWeeks = contrib.Weeks.Where(x => x.W >= filteredRange).ToList();
-                contrib.TotalAdditions = contribWeeks.Sum(x => x.A);
-                contrib.TotalDeletions = contribWeeks.Sum(x => x.D);
-                contrib.Total = contribWeeks.Sum(x => x.C);
-            }
-
-            var filteredContributors = gitHubContributors
-                .Where(g => !login.Contains(g.Author.Login))
-                .GroupBy(g => g.Author.Id)
-                .OrderByDescending(c => c.Sum(g => g.Total));
-
-            List<GitHubGlobalContributorModel> temp = new List<GitHubGlobalContributorModel>();
-
-            foreach (var group in filteredContributors)
-            {
-                var contributor = new GitHubGlobalContributorModel(group);
-                if (contributor.TotalCommits > 0)
-                {
-                    temp.Add(contributor);
-                }
-            }
-
-            return temp;
-
-
+        
         }
 
     }
+
 }
