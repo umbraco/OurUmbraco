638e8fc50a39ce5c517753b4f5f9e216e0377a41
Sebastiaan Jansssen <sebastiaan@umbraco.com>
2018-02-19 19:20:07 +0100

    Centralize access

---
+++ OurUmbraco/Our/UpcomingFeaturesService.cs
@@ -0,0 +1,16 @@
+﻿using OurUmbraco.Forum.Extensions;
+using Umbraco.Web;
+
+namespace OurUmbraco.Our
+{
+    public class UpcomingFeaturesService
+    {
+        public bool MemberHasAccessToFeature()
+        {
+            var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+            var member = umbracoHelper.MembershipHelper.GetCurrentMember();
+            var allowed = member != null && member.IsAdmin();
+            return allowed;
+        }
+    }
+}
