0976643beda9658933d9a31946992b0da0965bc6
Warren Buckley <warren@umbraco.com>
2018-02-23 08:15:01 +0000

    Adds in event handler at startup to try & connect to Gitter Relatime API which will proxy its data over to a SignalR hub so the UI can be updated in realtime on the homepage or other places on site

---
+++ OurUmbraco/Gitter/GitterProxyHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitterSharp.Model.Realtime;
+using Microsoft.AspNet.SignalR;
+
+namespace OurUmbraco.Gitter
+{
+    /// <summary>
+    /// This is a Simple SignalR Hub
+    /// That proxy's the Realtime events from Gitter's API to anyone connected to the our.umb site
+    /// Where we can display a total count of users online currently or similar
+    /// </summary>
+    public class GitterProxyHub : Hub
+    {
+        public void SendRealtimePresenceEvent(RealtimeUserPresence userPresence)
+        {
+            Clients.All.prescenceEvent(userPresence);
+        }
+
+        public void SendRealtimeRoomEvent(RealtimeRoomEvent roomEvent)
+        {
+            Clients.All.roomEvent(roomEvent);
+        }
+        
+        public void SendRealtimeUserEvent(RealtimeRoomUser roomUser)
+        {
+            Clients.All.userEvent(roomUser);
+        }
+        
+        public void SendRealtimeChatMessageEvent(RealtimeChatMessage chatMessage)
+        {
+            Clients.All.chatMessage(chatMessage);
+        }
+        
+    }
+}
---
+++ OurUmbraco/Gitter/GitterUmbracoEventHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Configuration;
+using GitterSharp.Services;
+using Umbraco.Core;
+
+namespace OurUmbraco.Gitter
+{
+    public class GitterUmbracoEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            //Gitter API token
+            var apiToken = ConfigurationManager.AppSettings["GitterApiToken"];
+
+            //The Room ID we want to listen for events from
+            var roomId = ConfigurationManager.AppSettings["GitterRoomId"];
+
+            //Register & setup/connect to Gitter Realtime API
+            var realtimeGitterService = new RealtimeGitterService(apiToken);
+
+            var proxyHub = new GitterProxyHub();
+
+            //Let's connect & listen...
+            realtimeGitterService.Connect();
+
+            //User presence
+            realtimeGitterService.SubscribeToUserPresence(roomId)
+                .Subscribe(x =>
+                {
+                    //Proxy the request with SignalR Hub
+                    proxyHub.SendRealtimePresenceEvent(x);
+                }, onError:OnError);
+
+
+            //Room Events
+            realtimeGitterService.SubscribeToRoomEvents(roomId)
+                .Subscribe(x =>
+                {
+                    proxyHub.SendRealtimeRoomEvent(x);
+                }, onError:OnError);
+
+
+            //Users in room
+            realtimeGitterService.SubscribeToRoomUsers(roomId)
+                .Subscribe(x =>
+                {
+                    proxyHub.SendRealtimeUserEvent(x);
+                }, onError:OnError);
+            
+
+            //Chat messages
+            realtimeGitterService.SubscribeToChatMessages(roomId)
+                .Subscribe(x =>
+                {
+                    proxyHub.SendRealtimeChatMessageEvent(x);
+                }, onError:OnError);
+        }
+
+        private void OnError(Exception exception)
+        {
+            //TODO: Log exception/warning
+            //Maybe connection issues?!
+
+            throw new NotImplementedException();
+        }
+    }
+}
