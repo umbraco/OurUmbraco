e67b2a5ec61caeedf9d3004f7ff9b690f3c47c8f
Warren Buckley <warren@umbraco.com>
2018-02-23 22:50:45 +0000

    Gitter Service - so that we can use it to get the last x nmber of messages from the room

---
+++ OurUmbraco/Gitter/GitterService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using GitterSharp.Model;
+using GitterSharp.Model.Requests;
+using GitterSharp.Services;
+
+namespace OurUmbraco.Gitter
+{
+    public class GitterService
+    {
+        private GitterApiService _gitterApi;
+        private string _roomId;
+
+        public GitterService()
+        {
+            _gitterApi = new GitterApiService(ConfigurationManager.AppSettings["GitterApiToken"]);
+            _roomId = ConfigurationManager.AppSettings["GitterRoomId"];
+        }
+
+        public Task<IEnumerable<Message>> GetMessages(int numberOfMessages = 2)
+        {
+            return _gitterApi.GetRoomMessagesAsync(_roomId, new MessageRequest {Limit = numberOfMessages});
+        }
+    }
+}
