8a0c1a20134c59a84c6c08f8d2ab18d56de00a72
Warren Buckley <warren@umbraco.com>
2018-03-05 14:20:38 +0000

    Adds in info window for marker click - that displays member name & avatar wrapped in a link to their profile

--- OurUmbraco.Site/Assets/js/map/community.map.js
+++ OurUmbraco.Site/Assets/js/map/community.map.js
@@ -11,12 +11,22 @@
     // create an array of markers based on a given "locations" array.
     // The map() method here has nothing to do with the Google Maps API.
     $.getJSON("/umbraco/api/mapapi/GetAllMemberLocations", function(data) {
-
+        
         var markers = data.map(function(item) {
             var latlng = new google.maps.LatLng(item.Lat, item.Lon);
-            return new google.maps.Marker({
+            var marker = new google.maps.Marker({
                 position: latlng
             });
+
+            var infowindow = new google.maps.InfoWindow({
+                content: "<a href='/member/" + item.Id + "' style='display:flex; align-items:center;'><img src='" + item.Avatar +"' title='" + item.Name + "' style='margin-right:5px;'/>" + item.Name + "</a>"
+            });
+
+            marker.addListener('click', function() {
+                infowindow.open(map, marker);
+            });
+
+            return marker;
         });
 
         // Add a marker clusterer to manage the markers.
