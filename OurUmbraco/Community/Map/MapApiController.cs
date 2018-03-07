2a850ffcc8b0c91922a9586453b148c77d3e47a9
Warren Buckley <warren@umbraco.com>
2018-03-07 09:03:35 +0000

    Update Examine Query to find members that are NOT blocked & members that have been approved

--- OurUmbraco/Community/Map/MapApiController.cs
+++ OurUmbraco/Community/Map/MapApiController.cs
@@ -15,25 +15,24 @@ namespace OurUmbraco.Community.Map
         { 
             var memberSearcher = ExamineManager.Instance.SearchProviderCollection["InternalMemberSearcher"];
             var searchCritera = memberSearcher.CreateSearchCriteria(IndexTypes.Member);
-            
+
             //Find all active members - where a lat & lon is set (with new NodeGathering field to index)
-            var query = searchCritera.Field("hasLocationSet", "1");
+            var query = searchCritera.Field("hasLocationSet", "1")
+                .And().Field("blocked", "0")
+                .And().Field("umbracoMemberApproved", "1");
 
-            //TODO: Check for members where blocked is false (0)
-            //And umbracoMemberApproved is true (1)
-            //I can't get to get it to work with the results I would expect currently
-
+            var rawQuery = query.ToString();
             var results = memberSearcher.Search(query.Compile());
 
             //Pluck the fields we need from the Examine index fields
             //For our much simpler model to send back as the JSON response
             var members = results.Select(result => new MemberLocation
             {
+                Id = result.Id,
                 Name = result.Fields["nodeName"],
-                Lat = result.Fields["latitude"],
-                Lon = result.Fields["longitude"],
                 Avatar = result.Fields["avatar"],
-                Id = result.Id
+                Lat = result.Fields["latitude"],
+                Lon = result.Fields["longitude"]
             })
             .ToList();
             
