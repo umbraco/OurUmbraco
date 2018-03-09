34cb635c9babaa69f91936e378a6cf8a27593273
Warren Buckley <warren@umbraco.com>
2018-03-09 11:54:50 +0000

    Use the google map idle event - from the NYC Subway example & prep to pass the params to our API Controller to get a subset of the markers based on the current viewport

--- OurUmbraco.Site/Assets/js/map/community.map.js
+++ OurUmbraco.Site/Assets/js/map/community.map.js
@@ -23,38 +23,43 @@
         });
     }
 
-    // Add some markers to the map.
-    // Note: The code uses the JavaScript Array.prototype.map() method to
-    // create an array of markers based on a given "locations" array.
-    // The map() method here has nothing to do with the Google Maps API.
-    $.getJSON("/umbraco/api/mapapi/GetAllMemberLocations", function(data) {
+    google.maps.event.addListener(map, 'idle', () => {
+        const sw = map.getBounds().getSouthWest();
+        const ne = map.getBounds().getNorthEast();
         
-        var markers = data.map(function(item) {
-            var latlng = new google.maps.LatLng(item.Lat, item.Lon);
-            var marker = new google.maps.Marker({
-                position: latlng
+        var url = "/umbraco/api/mapapi/GetAllMemberLocations?swLat=" + sw.lat() + "&swLon=" + sw.lng() + "&neLat=" + ne.lat() + "&neLon=" + ne.lng();
+
+        $.getJSON(url, function (data) {
+
+            var markers = data.map(function (item) {
+                var latlng = new google.maps.LatLng(item.Lat, item.Lon);
+                var marker = new google.maps.Marker({
+                    position: latlng
+                });
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: "<span>Loading</span>"
+                });
+
+                //Only render the HTML content - when you click the marker
+                var html = "<a href='/member/" + item.Id + "' style='display:flex; align-items:center;'><img src='" + item.Avatar + "' title='" + item.Name + "' style='margin-right:5px;'/>" + item.Name + "</a>";
+
+                marker.addListener('click', function () {
+                    infowindow.setContent(html);
+                    infowindow.open(map, marker);
+                });
+
+                return marker;
             });
 
-            var infowindow = new google.maps.InfoWindow({
-                content: "<span>Loading</span>"
-            });
+            // Add a marker clusterer to manage the markers.
+            var markerCluster = new MarkerClusterer(map,
+                markers,
+                {
+                    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+                });
 
-            //Only render the HTML content - when you click the marker
-            var html = "<a href='/member/" + item.Id + "' style='display:flex; align-items:center;'><img src='" + item.Avatar + "' title='" + item.Name + "' style='margin-right:5px;'/>" + item.Name + "</a>";
-
-            marker.addListener('click', function () {
-                infowindow.setContent(html);
-                infowindow.open(map, marker);
-            });
-
-            return marker;
         });
 
-        // Add a marker clusterer to manage the markers.
-        var markerCluster = new MarkerClusterer(map,
-            markers,
-            {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
     });
 };
