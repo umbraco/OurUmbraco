dcfe22e1d5a02396d2be8dbc7e16043c2176c737
Anders Bjerner <abjerner@skybrud.dk>
2018-05-03 20:57:05 +0200

    Blogs: Added POCO for a database RSS item

---
+++ OurUmbraco/Community/BlogPosts/BlogDatabaseItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Newtonsoft.Json;
+using Skybrud.Essentials.Json;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace OurUmbraco.Community.BlogPosts
+{
+
+    [TableName(TableName)]
+    [PrimaryKey(PrimaryKey, autoIncrement = false)]
+    [ExplicitColumns]
+    public class BlogDatabaseItem
+    {
+
+        public const string TableName = "CommunityBlogItems";
+
+        public const string PrimaryKey = "Id";
+
+        private BlogRssItem _data;
+
+        [Column(PrimaryKey)]
+        [PrimaryKeyColumn(AutoIncrement = false)]
+        public string Id { get; set; }
+
+        [Column("BlogId")]
+        public string BlogId { get; set; }
+
+        [Column("PubDate")]
+        public DateTime PublishedDate { get; set; }
+
+        [Column("Title")]
+        public string Title { get; set; }
+
+        [Column("Data")]
+        [SpecialDbType(SpecialDbTypes.NTEXT)]
+        public string DataRaw { get; set; }
+
+        [Ignore]
+        public BlogRssItem Data
+        {
+            get { return _data ?? JsonUtils.ParseJsonObject<BlogRssItem>(DataRaw); }
+            set { _data = value; DataRaw = JsonConvert.SerializeObject(_data, Formatting.None); }
+        }
+
+    }
+
+}
