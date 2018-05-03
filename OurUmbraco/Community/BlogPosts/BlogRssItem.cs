13d8bc09bb6e7ff418906c0d450f010bb47547d0
Anders Bjerner <abjerner@skybrud.dk>
2018-05-03 20:53:54 +0200

    Blogs: The "BlogRssItem" class should include "Guid" and "Description" properties with the values from the original RSS item
    - the "Guid" property can be used to identifying the RSS item when saved to the database.
    -t he "Description" property is useful as we can then search the description rather than just the title

--- OurUmbraco/Community/BlogPosts/BlogRssItem.cs
+++ OurUmbraco/Community/BlogPosts/BlogRssItem.cs
@@ -9,12 +9,23 @@ namespace OurUmbraco.Community.BlogPosts {
         [JsonProperty("title")]
         public string Title { get; set; }
 
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
+
         [JsonProperty("link")]
         public string Link { get; set; }
 
         [JsonProperty("pubDate")]
         [JsonConverter(typeof(IsoDateTimeConverter))]
         public DateTimeOffset PublishedDate { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        public bool HasDescription
+        {
+            get { return !String.IsNullOrWhiteSpace(Description); }
+        }
 
         [JsonProperty("channel")]
         public BlogRssChannel Channel { get; set; }
