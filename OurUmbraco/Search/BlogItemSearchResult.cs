7818ecf926866f04984bf3608127f4bc54d9c845
Anders Bjerner <abjerner@skybrud.dk>
2018-05-03 21:18:37 +0200

    Blogs: integrated blog posts in the global search

---
+++ OurUmbraco/Search/BlogItemSearchResult.cs
@@ -0,0 +1,23 @@
+﻿using Examine;
+using OurUmbraco.Community.BlogPosts;
+
+namespace OurUmbraco.Search {
+
+    public class BlogItemSearchResult {
+
+        public SearchResult Result { get; private set; }
+
+        public BlogRssItem Item { get; private set; }
+
+        public BlogInfo Blog { get; private set; }
+
+        public BlogItemSearchResult(SearchResult result, BlogRssItem item, BlogInfo blog)
+        {
+            Result = result;
+            Item = item;
+            Blog = blog;
+        }
+
+    }
+
+}
