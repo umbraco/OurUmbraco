ccc12d52fc38c5dccbd79c9730ffc1b06f931ce0
Damiaan Peeters <damiaan@comm-it.be>
2018-05-20 18:47:53 +0200

    Documentation Tag support with YAML

--- OurUmbraco/Our/Examine/ExamineHelper.cs
+++ OurUmbraco/Our/Examine/ExamineHelper.cs
@@ -7,10 +7,12 @@ using Examine;
 using Examine.LuceneEngine;
 using Umbraco.Core;
 using Umbraco.Core.Logging;
+using System.Text;
+using YamlDotNet.Serialization;
 
 namespace OurUmbraco.Our.Examine
 {
-    public static class ExamineHelper
+    public static partial class ExamineHelper
     {
         public const string DocumentationIndexer = "documentationIndexer";
 
@@ -45,10 +47,51 @@ namespace OurUmbraco.Our.Examine
             var lines = new List<string>();
             lines.AddRange(File.ReadAllLines(file.FullName));
 
-            var body = lines.Any()
-                ? RemoveSpecialCharacters(string.Join("", lines)).StripHtml()
-                : string.Empty;
+            // don't parse empty files
+            if (lines.Count == 0) { return simpleDataSet; }
 
+            // remove first empty lines
+            while (string.IsNullOrWhiteSpace(lines.ElementAt(0)))
+            {
+                lines.RemoveAt(0);
+            }
+
+            // Check if the first line is a YAML marker
+            // YAML is only accepted if it's on the top of the document
+            // because empty lines are already removed, the index needs to be 0
+            bool hasYaml = lines.ElementAt(0) == "---";
+            int secondYamlMarker = 0;
+
+            if (hasYaml)    
+            {
+                // Find the "next" triple dash starting from the second line
+                secondYamlMarker = lines.IndexOf("---", 1);
+            }
+
+            // add all yaml together and parse YAML meta data
+            YamlMetaData yamlMetaData = new YamlMetaData();
+            if (secondYamlMarker > 0)
+            {
+                var yamlInput = new StringBuilder();
+                for (int i = 0; i < secondYamlMarker; i++)
+                {
+                    yamlInput.AppendLine(lines.ElementAt(i));
+                };
+
+                var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(new YamlDotNet.Serialization.NamingConventions.CamelCaseNamingConvention())
+                    .IgnoreUnmatchedProperties()
+                    .Build();
+                yamlMetaData = deserializer.Deserialize<YamlMetaData>(yamlInput.ToString());
+            }
+
+            // build body 
+            var bodyLines = lines.GetRange(secondYamlMarker + 1, lines.Count - secondYamlMarker - 1);
+            var body = bodyLines.Any()
+                      ? RemoveSpecialCharacters(string.Join(" ", bodyLines)).StripHtml()
+                      : string.Empty;
+
+            // extract the first headline
             var firstHeadline = lines.FirstOrDefault(x => x.StartsWith("#"));
             var headLine = firstHeadline ?? file.FullName.Substring(file.FullName.LastIndexOf("\\", StringComparison.Ordinal) + 1).Replace(".md", string.Empty).Replace("-", " ");
 
@@ -60,15 +103,20 @@ namespace OurUmbraco.Our.Examine
             simpleDataSet.RowData.Add("updateDate", file.CreationTime.ToString("yyyy-MM-dd HH:mm:ss"));
             simpleDataSet.RowData.Add("nodeTypeAlias", "documentation");
 
-            simpleDataSet.RowData.Add("dateCreated", file.CreationTime.ToString("yyyy-MM-dd HH:mm:ss"));          
-            
+            simpleDataSet.RowData.Add("dateCreated", file.CreationTime.ToString("yyyy-MM-dd HH:mm:ss"));
+
             //TODO: This will always be exactly the same since results all files are written at the same time IIRC
             simpleDataSet.RowData.Add("Path", file.FullName);
             simpleDataSet.RowData.Add("searchAblePath", file.FullName.Replace("\\", " ").Replace(":", ""));
             simpleDataSet.RowData.Add("url", BuildUrl(file.FullName));
-            
+
+            // Add Yaml stuff
+            simpleDataSet.RowData.Add("tags", yamlMetaData.Tags);
+            simpleDataSet.RowData.Add("versionFrom", yamlMetaData.VersionFrom);
+            simpleDataSet.RowData.Add("versionTo", yamlMetaData.VersionTo);
+
             // Used to exclude broken link page from the search results.
-            if(file.Name == "broken-link.md")
+            if (file.Name == "broken-link.md")
             {
                 simpleDataSet.RowData.Clear();
             }
