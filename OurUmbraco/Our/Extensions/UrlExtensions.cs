085430ca4ab9a2d03c30e5b405cee934a268adf7
Anders Bjerner <abjerner@skybrud.dk>
2018-05-20 12:49:29 +0200

    GitHub usernames: Introduced new UrlExtensions class for determining the profile URL of a member

---
+++ OurUmbraco/Our/Extensions/UrlExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Web.Mvc;
+using OurUmbraco.Our.Models;
+
+namespace OurUmbraco.Our.Extensions
+{
+
+    public static class UrlExtensions
+    {
+
+        public static string GetProfileUrl(this UrlHelper helper, MemberData member)
+        {
+            return "/members/" + (member.HasGitHubUsername ? member.GitHubUsername : "id:" + member.Id);
+        }
+
+    }
+
+}
