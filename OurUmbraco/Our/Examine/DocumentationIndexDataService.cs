d2472311484039a60d277ff50e77ba7a4e0e0b37
Damiaan Peeters <damiaan@comm-it.be>
2018-05-21 05:18:16 +0200

    Skip file if an exception occurred & add logging

--- OurUmbraco/Our/Examine/DocumentationIndexDataService.cs
+++ OurUmbraco/Our/Examine/DocumentationIndexDataService.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Web.Hosting;
 using Examine;
 using Examine.LuceneEngine;
+using System;
 
 namespace OurUmbraco.Our.Examine
 {
@@ -19,8 +20,8 @@ namespace OurUmbraco.Our.Examine
 
             var directory = new DirectoryInfo(fullPath);
 
-            var files = config.Recursive 
-                ? directory.GetFiles(config.SupportedFileTypes, SearchOption.AllDirectories) 
+            var files = config.Recursive
+                ? directory.GetFiles(config.SupportedFileTypes, SearchOption.AllDirectories)
                 : directory.GetFiles(config.SupportedFileTypes);
 
             var i = 0; //unique id for each doc
@@ -29,10 +30,21 @@ namespace OurUmbraco.Our.Examine
             {
                 i++;
                 var simpleDataSet = new SimpleDataSet { NodeDefinition = new IndexedNode(), RowData = new Dictionary<string, string>() };
-                simpleDataSet = ExamineHelper.MapFileToSimpleDataIndexItem(file, simpleDataSet, i, indexType);
 
+                try
+                {
+                    simpleDataSet = ExamineHelper.MapFileToSimpleDataIndexItem(file, simpleDataSet, i, indexType);
+                }
+                catch (Exception ex)
+                {
+                    Umbraco.Core.Logging.LogHelper.Error<DocumentationIndexDataService>(
+                        $"Indexing docs - could not parse document {file.FullName}", ex);
+                    if (System.Diagnostics.Debugger.IsAttached) System.Diagnostics.Debugger.Break();
+                }
                 yield return simpleDataSet;
             }
+            Umbraco.Core.Logging.LogHelper.Info<DocumentationIndexDataService>(
+                        $"Indexed documentation files: {0}", () => files.Length);
         }
     }
 }
