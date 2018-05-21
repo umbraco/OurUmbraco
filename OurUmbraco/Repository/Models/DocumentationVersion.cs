8572dd0065f8b6b8d04f91dc2ab4c53dc752bcc5
Jeavon Leopold <jeavon@crumpled-dog.com>
2018-05-22 00:11:49 +0200

    WIP - displaying current and alternative versions of docs

---
+++ OurUmbraco/Repository/Models/DocumentationVersion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurUmbraco.Repository.Models
+{
+    public class DocumentationVersion
+    {
+        public string Path { get; set; }
+        public string Version { get; set; }
+        public string Url { get; set; }
+        public bool IsCurrentVersion { get; set; }
+    }
+}
