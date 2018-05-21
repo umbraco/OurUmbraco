44396f03ed21852f9a0ff7fb14f3fa5236087cdb
Carole Rennie Logan <crgrieve@users.noreply.github.com>
2018-05-21 11:34:06 +0100

    retrieve feed logic

---
+++ OurUmbraco/HighFiveFeed/Models/HighFiveResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurUmbraco.HighFiveFeed.Models
+{
+    public class HighFiveResponse
+    {
+        public int Id { get; set; }
+        public string from{ get; set; }
+        public string to { get; set; }
+        public string fromAvatarUrl { get; set; }
+        public string toAvatarUrl { get; set; }
+        public string type { get; set; }
+        public string url { get; set; }
+
+    }
+}
