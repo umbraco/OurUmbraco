04b459f47013d083f7fa602c839dd3f5c4dd8313
Damiaan Peeters <damiaan@comm-it.be>
2018-05-21 11:51:53 +0200

    Add MajorVersion to examine documentation index

--- OurUmbraco/Our/Examine/ExamineHelper.cs
+++ OurUmbraco/Our/Examine/ExamineHelper.cs
@@ -10,6 +10,8 @@ using Umbraco.Core.Logging;
 using System.Text;
 using YamlDotNet.Serialization;
 using OurUmbraco.Documentation.Busineslogic;
+using AngleSharp;
+using System.Configuration;
 
 namespace OurUmbraco.Our.Examine
 {
@@ -61,34 +63,7 @@ namespace OurUmbraco.Our.Examine
                 lines.RemoveAt(0);
             }
 
-            // Check if the first line is a YAML marker
-            // YAML is only accepted if it's on the top of the document
-            // because empty lines are already removed, the index needs to be 0
-            bool hasYaml = lines.ElementAt(0) == "---";
-            int secondYamlMarker = 0;
-
-            if (hasYaml)
-            {
-                // Find the "next" triple dash starting from the second line
-                secondYamlMarker = lines.IndexOf("---", 1);
-            }
-
-            // add all yaml together and parse YAML meta data
-            YamlMetaData yamlMetaData = new YamlMetaData();
-            if (secondYamlMarker > 0)
-            {
-                var yamlInput = new StringBuilder();
-                for (int i = 1; i < secondYamlMarker; i++)
-                {
-                    yamlInput.AppendLine(lines.ElementAt(i));
-                };
-
-                var deserializer = new DeserializerBuilder()
-                    .WithNamingConvention(new YamlDotNet.Serialization.NamingConventions.CamelCaseNamingConvention())
-                    .IgnoreUnmatchedProperties()
-                    .Build();
-                yamlMetaData = deserializer.Deserialize<YamlMetaData>(yamlInput.ToString());
-            }
+            int secondYamlMarker = AddYamlFields(simpleDataSet, lines);
 
             // build body 
             var bodyLines = lines.GetRange(secondYamlMarker + 1, lines.Count - secondYamlMarker - 1);
@@ -115,17 +90,73 @@ namespace OurUmbraco.Our.Examine
             simpleDataSet.RowData.Add("searchAblePath", file.FullName.Replace("\\", " ").Replace(":", ""));
             simpleDataSet.RowData.Add("url", BuildUrl(file.FullName));
 
-            // Add Yaml stuff
-            simpleDataSet.RowData.Add("tags", yamlMetaData.Tags);
-            simpleDataSet.RowData.Add("versionFrom", yamlMetaData.VersionFrom);
-            simpleDataSet.RowData.Add("versionTo", yamlMetaData.VersionTo);
-
             // Used to exclude broken link page from the search results.
             if (file.Name == "broken-link.md")
             {
                 simpleDataSet.RowData.Clear();
             }
             return simpleDataSet;
+        }
+
+        private static int AddYamlFields(SimpleDataSet simpleDataSet, List<string> lines)
+        {
+            // Check if the first line is a YAML marker
+            // YAML is only accepted if it's on the top of the document
+            // because empty lines are already removed, the index needs to be 0
+            bool hasYaml = lines.ElementAt(0) == "---";
+            int secondYamlMarker = 0;
+
+            if (hasYaml)
+            {
+                // Find the "next" triple dash starting from the second line
+                secondYamlMarker = lines.IndexOf("---", 1);
+
+
+                // add all yaml together and parse YAML meta data
+                YamlMetaData yamlMetaData = new YamlMetaData();
+                if (secondYamlMarker > 0)
+                {
+                    var yamlInput = new StringBuilder();
+                    for (int i = 1; i < secondYamlMarker; i++)
+                    {
+                        yamlInput.AppendLine(lines.ElementAt(i));
+                    };
+
+                    var deserializer = new DeserializerBuilder()
+                        .WithNamingConvention(new YamlDotNet.Serialization.NamingConventions.CamelCaseNamingConvention())
+                        .IgnoreUnmatchedProperties()
+                        .Build();
+                    yamlMetaData = deserializer.Deserialize<YamlMetaData>(yamlInput.ToString());
+                }
+                // Add Yaml stuff
+                simpleDataSet.RowData.Add("tags", yamlMetaData.Tags);
+                simpleDataSet.RowData.Add("versionFrom", yamlMetaData.VersionFrom);
+                simpleDataSet.RowData.Add("versionTo", yamlMetaData.VersionTo);
+
+                var matchingMajorVersions = CalculateMajorVersions(yamlMetaData);
+                simpleDataSet.RowData.Add("majorVersion", string.Join(" ", matchingMajorVersions));
+            }
+
+            return secondYamlMarker;
+        }
+
+        private static IEnumerable<int> CalculateMajorVersions(YamlMetaData yamlMetaData)
+        {
+            // Try to find out which major versions are supported
+            var currentDocVersion = int.Parse(ConfigurationManager.AppSettings["Documentation:CurrentMajorVersion"]);
+            var semverCurrent = new Semver.SemVersion(currentDocVersion);
+            bool hasFrom = Semver.SemVersion.TryParse(yamlMetaData.VersionFrom, out Semver.SemVersion semverFrom);
+            bool hasTo = Semver.SemVersion.TryParse(yamlMetaData.VersionTo, out Semver.SemVersion semverTo);
+
+            if (hasFrom == false) { semverFrom = new Semver.SemVersion(currentDocVersion); }
+            if (hasTo == false) { semverTo = semverFrom < semverCurrent ? semverCurrent : semverFrom; }
+            if (semverFrom > semverTo) { semverFrom = semverTo; }
+
+            var matchingMajorVersions = new List<int>();
+            for (int i = semverFrom.Major; i <= semverTo.Major; i++)
+            {
+                yield return i;
+            }
         }
 
         private static string BuildUrl(string path)
