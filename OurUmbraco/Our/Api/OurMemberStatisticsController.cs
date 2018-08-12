9fc665fe705b59aa432a78f735cac3dd054b95d8
Sebastiaan Janssen <sebastiaan@umbraco.com>
2018-08-12 20:14:29 +0200

    Members who have earned karma at least once

--- OurUmbraco/Our/Api/OurMemberStatisticsController.cs
+++ OurUmbraco/Our/Api/OurMemberStatisticsController.cs
@@ -26,12 +26,32 @@ namespace OurUmbraco.Our.Api
             var totalNonSpamMembers = totalMembers - numberBlocked;
             var totalNonSpamMembersWithLogin = totalMembers - numberBlocked - neverLoggedIn;
 
+            var sql = @"SELECT DISTINCT(memberId) FROM";
+            var commentMemberIds = ApplicationContext.DatabaseContext.Database.Fetch<int>($"{sql} powersComment");
+            var projectMemberIds = ApplicationContext.DatabaseContext.Database.Fetch<int>($"{sql} powersProject");
+            var projectVersionMemberIds = ApplicationContext.DatabaseContext.Database.Fetch<int>($"{sql} powersProjectVersion");
+            var topicMemberIds = ApplicationContext.DatabaseContext.Database.Fetch<int>($"{sql} powersTopic");
+            var wikiMemberIds = ApplicationContext.DatabaseContext.Database.Fetch<int>($"{sql} powersWiki");
+            var contributorMemberIds = ApplicationContext.DatabaseContext.Database.Fetch<int>($"{sql} projectContributors");
+            var downloadMemberIds = ApplicationContext.DatabaseContext.Database.Fetch<int>($"{sql} projectDownload");
+
+            var uniqueMemberIds = commentMemberIds
+                .Union(projectMemberIds)
+                .Union(projectVersionMemberIds)
+                .Union(topicMemberIds)
+                .Union(wikiMemberIds)
+                .Union(contributorMemberIds)
+                .Union(downloadMemberIds).Count();
+
+            // get create date for members: SELECT createDate FROM umbracoNode WHERE nodeObjectType = '39EB0F98-B348-42A1-8662-E7EB18487560'
+
             var statistics = new OurMemberStatistics
             {
                 TotalMembers = totalMembers,
                 TotalNonSpamMembers = totalNonSpamMembers,
                 TotalNonSpamMembersWithLogin = totalNonSpamMembersWithLogin,
-                TotalBlockedMembers = numberBlocked
+                TotalBlockedMembers = numberBlocked,
+                TotalMembersEarningKarma = uniqueMemberIds
             };
             
             return statistics;
@@ -44,5 +64,6 @@ namespace OurUmbraco.Our.Api
         public int TotalNonSpamMembers { get; set; }
         public int TotalNonSpamMembersWithLogin { get; set; }
         public int TotalBlockedMembers { get; set; }
+        public int TotalMembersEarningKarma { get; set; }
     }
 }
