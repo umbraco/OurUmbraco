df07929b1f9a9f667aa84784e4047a072ab60b76
Sebastiaan Janssen <sebastiaan@umbraco.com>
2018-08-25 12:01:04 +0200

    Standardize the display of the release date

---
+++ OurUmbraco/Our/Extensions/DateTimeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OurUmbraco.Our.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string AsDisplayedReleaseDate(this DateTime releaseDate)
+        {
+            return releaseDate == default(DateTime) ? "Not yet determined" : releaseDate.ToString("dddd, MMMM d yyyy");
+        }
+    }
+}
