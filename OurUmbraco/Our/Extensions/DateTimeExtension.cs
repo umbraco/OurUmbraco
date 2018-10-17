5329afcfa191cb8f90d9e6f4770d4d8a481f12cb
Sebastiaan Janssen <sebastiaan@umbraco.com>
2018-10-17 15:28:34 +0200

    Update stats with unique contributors this and last month

--- OurUmbraco/Our/Extensions/DateTimeExtension.cs
+++ OurUmbraco/Our/Extensions/DateTimeExtension.cs
@@ -9,7 +9,6 @@ namespace OurUmbraco.Our.Extensions
             return releaseDate == default(DateTime) ? "Not yet determined" : releaseDate.ToString("dddd, MMMM d yyyy");
         }
 
-
         public static int MonthsBetween(this DateTime fromDate, DateTime toDate)
         {
             if (fromDate > toDate)
@@ -18,6 +17,13 @@ namespace OurUmbraco.Our.Extensions
             var monthDiff = Math.Abs((toDate.Year * 12 + (toDate.Month - 1)) - (fromDate.Year * 12 + (fromDate.Month - 1)));
 
             return fromDate.AddMonths(monthDiff) > toDate || toDate.Day < fromDate.Day ? monthDiff - 1 : monthDiff;
+        }
+
+        public static DateTime GetDateLastDayOfMonth(this DateTime dateTime)
+        {
+            var lastDay = DateTime.DaysInMonth(dateTime.Year, dateTime.Month);
+            var returnDate = new DateTime(dateTime.Year, dateTime.Month, lastDay);
+            return returnDate;
         }
     }
 }
