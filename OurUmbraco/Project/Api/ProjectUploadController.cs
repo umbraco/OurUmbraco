a21927e977e15d0ba3637d1e5b1b1ccde4ed1e41
Niels Hartvig <nh@umbraco.dk>
2019-11-07 15:39:07 +0000

    Add support for uploading packages

--- OurUmbraco/Project/Api/ProjectUploadController.cs
+++ OurUmbraco/Project/Api/ProjectUploadController.cs
@@ -1,7 +1,16 @@
-﻿using OurUmbraco.Auth;
+﻿using Newtonsoft.Json;
+using OurUmbraco.Auth;
 using OurUmbraco.Wiki.BusinessLogic;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.EntityBase;
 
 namespace OurUmbraco.Project.Api
 {
@@ -26,5 +35,71 @@ namespace OurUmbraco.Project.Api
 
 
         // TODO: Hartvig does his magic for upload
+        [HttpPost]
+        public async Task<HttpResponseMessage> UpdatePackage()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                // Read the form data.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                // verify that a file exist (and only one)
+                if (provider.FileData.Count == 0)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "No file attached");
+                }
+                else if (provider.FileData.Count > 1)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "You can only upload one file");
+                }
+                else
+                {
+                    var packageFile = provider.FileData.FirstOrDefault();
+                    var fileName = packageFile.Headers.ContentDisposition.FileName.Replace("\"", "");
+                    var packageFileExtension = Path.GetExtension(fileName);
+                    var fileType = provider.FormData["fileType"];
+                    var dotNetVersion = provider.FormData["dotNetVersion"];
+                    var umbracoVersions = JsonConvert.DeserializeObject<List<UmbracoVersion>>(provider.FormData["umbracoVersions"]);
+                    var isCurrent = bool.Parse(provider.FormData["isCurrent"]);
+
+                    // get package guid from id
+                    IContent packageEntity = ApplicationContext.Services.ContentService.GetById(ProjectNodeId);
+                    if (packageEntity != null)
+                    {
+                        var packageVersion = packageEntity.Version;
+
+                        // create file
+                        var file = WikiFile.Create(
+                        fileName,
+                        packageFileExtension,
+                        packageVersion,
+                        AuthorisedMember.Key,
+                        System.IO.File.ReadAllBytes(packageFile.LocalFileName),
+                        fileType,
+                        umbracoVersions);
+
+                        file.Current = isCurrent;
+
+                        return Request.CreateResponse(HttpStatusCode.OK, "Package file updated");
+                    }
+                }
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+
+            throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+        }
     }
 }
