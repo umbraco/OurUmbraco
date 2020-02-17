3041a4c34c116d917e75f97674b3522b545107a4
elitsa <elm@umbraco.dk>
2020-02-17 09:13:40 +0100

    Adding endpoint for checking if upgrade is available. Creating a model that will contain the information for an upgrade. The response from the endpoint will contain this model serialized to json

--- OurUmbraco/Our/Api/UpgradeCheckController.cs
+++ OurUmbraco/Our/Api/UpgradeCheckController.cs
@@ -1,6 +1,12 @@
-﻿using System.Web;
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using System.Web.Security;
+using Newtonsoft.Json;
 using OurUmbraco.Our.Models;
+using OurUmbraco.Our.Services;
 using Umbraco.Core.Persistence;
 using Umbraco.Web.WebApi;
 
@@ -103,6 +109,110 @@ namespace OurUmbraco.Our.Api
             }
 
             return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CheckUpgrade(UpgradeModel model)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(Request.CreateValidationErrorResponse(ModelState));
+
+            int revision;
+            int.TryParse(model.VersionComment, out revision);
+            var version = new System.Version(model.VersionMajor, model.VersionMinor, model.VersionPatch, revision);
+
+            var latestVersion = GetLatestVersionFromMajor(model.VersionMajor);
+            if (latestVersion == null)
+                return Ok(JsonConvert.SerializeObject(new UpgradeResultModel(UpgradeType.None, "", "")));
+
+            // Persist the update check for our statistics, don't remove!
+            var caller = HttpContext.Current.Request.UserHostName + "_" +
+                         HttpContext.Current.Request.UserHostAddress;
+            var callerHashed = FormsAuthentication.HashPasswordForStoringInConfigFile(caller, "sha1");
+            PersistUpdateCheck(callerHashed, model.VersionMajor, model.VersionMinor, model.VersionPatch,
+                model.VersionComment);
+            // End of persisting the update check for our statistics, don't remove!
+
+            // Special case for 4.0.4.2, apperently we never recommended them to upgrade
+            if (version == new System.Version(4, 0, 4, 2) || version == latestVersion)
+                return Ok(JsonConvert.SerializeObject(new UpgradeResultModel(UpgradeType.None, "", "")));
+
+            if (version.Major == 4)
+            {
+                // We had some special cases in the old updatechecker so I left them here
+                if (version == new System.Version(4, 0, 4, 1))
+                    return Ok(JsonConvert.SerializeObject(new UpgradeResultModel(UpgradeType.Minor, "4.0.4.2 is out with fixes for load-balanced sites.", "http://our.umbraco.org/download")));
+
+                if (version == new System.Version(4, 0, 4, 0))
+                    return Ok(JsonConvert.SerializeObject(new UpgradeResultModel(UpgradeType.Critical, "4.0.4.1 is out fixing a serious macro bug. Please upgrade!", "http://our.umbraco.org/download")));
+
+
+                if (version == new System.Version(4, 7, 0, 0))
+                    return Ok(JsonConvert.SerializeObject(new UpgradeResultModel(UpgradeType.Critical, "This Umbraco installation needs to be upgraded. It may contain a potential security issue!", "http://our.umbraco.org/download")));
+
+
+                if (version >= new System.Version(4, 10, 0, 0) && version < latestVersion)
+                    return Ok(JsonConvert.SerializeObject(new UpgradeResultModel(UpgradeType.Major, $"{latestVersion} is released. Upgrade today - it's free!", "http://our.umbraco.org/download")));
+            }
+
+            if (version.Major == 6)
+            {
+                if ((version < latestVersion))
+                    return Ok(JsonConvert.SerializeObject(new UpgradeResultModel(UpgradeType.Major, $"{latestVersion} is released. Upgrade today - it's free!", "http://our.umbraco.org/download")));
+            }
+
+            if (version.Major == 7 || version.Major == 8 || version.Major > 8)
+            {
+                if (version < latestVersion)
+                    return Ok(JsonConvert.SerializeObject(new UpgradeResultModel(UpgradeType.Minor, $"{latestVersion} is released. Upgrade today - it's free!", $"http://our.umbraco.org/contribute/releases/{latestVersion.Major}{latestVersion.Minor}{latestVersion.Build}")));
+            }
+
+            return ResponseMessage(new HttpResponseMessage(HttpStatusCode.NoContent));
+        }
+
+        private void PersistUpdateCheck(string server, int majorVersion, int minorVersion, int patchVersion, string commentVersion)
+        {
+            if (commentVersion.Length > 49)
+            {
+                commentVersion = commentVersion.Substring(0, 47) + "...";
+            }
+
+            using (var umbracoUpdateDb = new Database("umbracoUpdate"))
+            {
+                var insertCmd = @"EXEC [UpdatePing]
+           @pinger, @major, @minor, @patch, @comment";
+
+                umbracoUpdateDb.Execute(insertCmd, new
+                {
+                    pinger = server,
+                    major = majorVersion,
+                    minor = minorVersion,
+                    patch = patchVersion,
+                    comment = commentVersion
+                });
+            }
+        }
+
+        private System.Version GetLatestVersionFromMajor(int major)
+        {
+            var releasesService = new ReleasesService();
+            var releases = releasesService.GetReleasesCache().Where(x => x.Released).OrderByDescending(x => x.FullVersion).ToList();
+            var majorVersionGroups = releases.GroupBy(x => x.FullVersion.Major).OrderByDescending(x => x.Key);
+
+            foreach (var majorVersion in majorVersionGroups)
+            {
+                if (majorVersion.Key == major)
+                {
+                    var releasesInGroup = releases.Where(x => x.FullVersion.Major == majorVersion.Key).ToList();
+                    var newestReleaseInGroup = releasesInGroup.First().FullVersion;
+                    var revision = (newestReleaseInGroup.Revision <= 0) ? 0 : newestReleaseInGroup.Revision;
+
+                    return new System.Version(newestReleaseInGroup.Major, newestReleaseInGroup.Minor,
+                        newestReleaseInGroup.Build, revision);
+                }
+            }
+
+            return null;
         }
     }
 }
---
+++ OurUmbraco/Our/Models/UpgradeResultModel.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace OurUmbraco.Our.Models
+{
+    public enum UpgradeType
+    {
+        None,
+        Patch,
+        Minor,
+        Major,
+        Critical,
+        Error,
+        OutOfSync
+    }
+
+    public class UpgradeResultModel
+    {
+        [JsonProperty(PropertyName = "upgradeType")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public UpgradeType UpgradeType { get; set; }
+        [JsonProperty(PropertyName = "comment")]
+        public string Comment { get; set; }
+        [JsonProperty(PropertyName = "upgradeUrl")]
+        public string UpgradeUrl { get; set; }
+
+        public UpgradeResultModel(UpgradeType upgradeType, string comment, string upgradeUrl)
+        {
+            UpgradeType = upgradeType;
+            Comment = comment;
+            UpgradeUrl = upgradeUrl;
+        }
+    }
+}
