0ea9cdfe6f6ce298fd76995e3fc49ee7af9e9ee7
James Wilkinson <james.wilkinson@sigma.se>
2020-04-11 21:37:04 +0100

    Added try-catch to handle Invalid responses to aid debugging

--- OurUmbraco/Community/Videos/CommunityVideosService.cs
+++ OurUmbraco/Community/Videos/CommunityVideosService.cs
@@ -96,103 +96,111 @@ namespace OurUmbraco.Community.Videos
 
         public void UpdateYouTubePlaylistVideo(string playlistId)
         {
-
-            // Make sure we have a TEMP directory
-            string dir = IOHelper.MapPath("~/App_Data/TEMP/YouTube/");
-            Directory.CreateDirectory(dir);
-
-            // Make an initial request to get information about the playlist
-            var response1 = Api.YouTube.Playlists.GetPlaylists(new YouTubeGetPlaylistListOptions
+            try
             {
-                Ids = new[] { playlistId }
-            });
+                // Make sure we have a TEMP directory
+                string dir = IOHelper.MapPath("~/App_Data/TEMP/YouTube/");
+                Directory.CreateDirectory(dir);
 
-            // Get a reference to the playlist (using "Single" as there should be exactly one playlist)
-            var playlist = response1.Body.Items.Single();
-
-            // Save the playlist to the disk
-            JsonUtils.SaveJsonObject(dir + "Playlist_" + playlist.Id + ".json", playlist);
-
-            // List of all video IDs
-            List<string> ids = new List<string>();
-
-            // Initialize the options for getting the playlist items
-            var playlistItemsOptions = new YouTubeGetPlaylistItemListOptions(playlistId)
-            {
-
-                // Maximum allowed value is 50
-                MaxResults = 50
-
-            };
-
-            int page = 0;
-            while (page < 10)
-            {
-
-                // Get the playlist items
-                var response2 = Api.YouTube.PlaylistItems.GetPlaylistItems(playlistItemsOptions);
-
-                // Append each video ID to the list
-                foreach (var item in response2.Body.Items)
+                // Make an initial request to get information about the playlist
+                var response1 = Api.YouTube.Playlists.GetPlaylists(new YouTubeGetPlaylistListOptions
                 {
-                    ids.Add(item.VideoId);
-                }
+                    Ids = new[] {playlistId}
+                });
 
-                // Break the loop if there are no additional pages
-                if (String.IsNullOrWhiteSpace(response2.Body.NextPageToken))
+                // Get a reference to the playlist (using "Single" as there should be exactly one playlist)
+                var playlist = response1.Body.Items.Single();
+
+                // Save the playlist to the disk
+                JsonUtils.SaveJsonObject(dir + "Playlist_" + playlist.Id + ".json", playlist);
+
+                // List of all video IDs
+                List<string> ids = new List<string>();
+
+                // Initialize the options for getting the playlist items
+                var playlistItemsOptions = new YouTubeGetPlaylistItemListOptions(playlistId)
                 {
-                    break;
-                }
 
-                // Update the options with the page token
-                playlistItemsOptions.PageToken = response2.Body.NextPageToken;
+                    // Maximum allowed value is 50
+                    MaxResults = 50
 
-                page++;
-
-            }
-
-            // Iterate through groups of IDs (maximum 50 items per group)
-            foreach (var group in ids.Where(x => !ExistsOnDiskAndIsUpToDate(x)).InGroupsOf(50))
-            {
-
-                // Initialize the video options
-                var videosOptions = new YouTubeGetVideoListOptions
-                {
-                    Ids = group.ToArray(),
-                    Part = YouTubeVideoParts.Snippet + YouTubeVideoParts.ContentDetails + YouTubeVideoParts.Statistics
                 };
 
-                // Make a request to the APi to get video information
-                var res3 = Api.YouTube.Videos.GetVideos(videosOptions);
-
-                // Iterate through the videos
-                foreach (var video in res3.Body.Items)
+                int page = 0;
+                while (page < 10)
                 {
 
-                    // Save the video to the disk
-                    string path = dir + "Video_" + video.Id + ".json";
-                    JsonUtils.SaveJsonObject(path, video);
+                    // Get the playlist items
+                    var response2 = Api.YouTube.PlaylistItems.GetPlaylistItems(playlistItemsOptions);
 
-                    // Download the thumnails for each video
-                    var thumbnailUrl = GetThumbnail(video).Url;
+                    // Append each video ID to the list
+                    foreach (var item in response2.Body.Items)
+                    {
+                        ids.Add(item.VideoId);
+                    }
 
-                    const string mediaRoot = "~/media/YouTube";
-                    var thumbnailFile = IOHelper.MapPath($"{mediaRoot}/{video.Id}.jpg");
+                    // Break the loop if there are no additional pages
+                    if (String.IsNullOrWhiteSpace(response2.Body.NextPageToken))
+                    {
+                        break;
+                    }
 
-                    var mediaPath = IOHelper.MapPath(mediaRoot);
-                    if (Directory.Exists(mediaPath) == false)
-                        Directory.CreateDirectory(mediaPath);
+                    // Update the options with the page token
+                    playlistItemsOptions.PageToken = response2.Body.NextPageToken;
 
-                    if (File.Exists(thumbnailFile))
-                        continue;
+                    page++;
 
-                    using (var client = new WebClient())
-                        client.DownloadFile(thumbnailUrl, thumbnailFile);
                 }
-            }
 
-            // Load the videos from the individual files, and save them to a common file
-            JsonUtils.SaveJsonArray(dir + "Playlist_" + playlistId + "_Videos.json", ids.Select(LoadYouTubeVideo).WhereNotNull());
+                // Iterate through groups of IDs (maximum 50 items per group)
+                foreach (var group in ids.Where(x => !ExistsOnDiskAndIsUpToDate(x)).InGroupsOf(50))
+                {
+
+                    // Initialize the video options
+                    var videosOptions = new YouTubeGetVideoListOptions
+                    {
+                        Ids = group.ToArray(),
+                        Part = YouTubeVideoParts.Snippet + YouTubeVideoParts.ContentDetails +
+                               YouTubeVideoParts.Statistics
+                    };
+
+                    // Make a request to the APi to get video information
+                    var res3 = Api.YouTube.Videos.GetVideos(videosOptions);
+
+                    // Iterate through the videos
+                    foreach (var video in res3.Body.Items)
+                    {
+
+                        // Save the video to the disk
+                        string path = dir + "Video_" + video.Id + ".json";
+                        JsonUtils.SaveJsonObject(path, video);
+
+                        // Download the thumnails for each video
+                        var thumbnailUrl = GetThumbnail(video).Url;
+
+                        const string mediaRoot = "~/media/YouTube";
+                        var thumbnailFile = IOHelper.MapPath($"{mediaRoot}/{video.Id}.jpg");
+
+                        var mediaPath = IOHelper.MapPath(mediaRoot);
+                        if (Directory.Exists(mediaPath) == false)
+                            Directory.CreateDirectory(mediaPath);
+
+                        if (File.Exists(thumbnailFile))
+                            continue;
+
+                        using (var client = new WebClient())
+                            client.DownloadFile(thumbnailUrl, thumbnailFile);
+                    }
+                }
+
+                // Load the videos from the individual files, and save them to a common file
+                JsonUtils.SaveJsonArray(dir + "Playlist_" + playlistId + "_Videos.json",
+                    ids.Select(LoadYouTubeVideo).WhereNotNull());
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error<CommunityVideosService>("Could not get data from YouTube", ex);
+            }
         }
 
         private static YouTubeVideoThumbnail GetThumbnail(YouTubeVideo video)
