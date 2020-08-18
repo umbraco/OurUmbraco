14cfecf86511a137c7dc24d32900d5632759a153
hartviglarsen <hartvig@pm.me>
2020-08-18 13:02:43 +0200

    Add Karma endpoint

---
+++ OurUmbraco/Community/Karma/Controllers/Http/KarmaController.cs
@@ -0,0 +1,22 @@
+﻿using OurUmbraco.Community.Controllers;
+using Umbraco.Web.WebApi;
+
+namespace OurUmbraco.Community.Karma.Controllers.Http
+{
+    public class KarmaController : UmbracoApiController
+    {
+        private readonly KarmaService _karmaService;
+
+        public KarmaController()
+        {
+            _karmaService = new KarmaService();
+        }
+
+        public KarmaStatistics GetKarmaStatistics()
+        {
+            var stats = _karmaService.GetCachedKarmaStatistics();
+
+            return stats;
+        }
+    }
+}
