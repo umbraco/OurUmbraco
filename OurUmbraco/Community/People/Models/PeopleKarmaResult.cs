1d8e9ff22f4175498bbfc99bc0faaba271f2f40b
hartviglarsen <hartvig@pm.me>
2020-08-18 13:02:35 +0200

    Update KarmaService

--- OurUmbraco/Community/People/Models/PeopleKarmaResult.cs
+++ OurUmbraco/Community/People/Models/PeopleKarmaResult.cs
@@ -4,6 +4,7 @@
     {
         public string MemberName { get; set; }
         public string MemberId { get; set; }
+        public string MemberAvatarUrl { get; set; }
         public int Performed { get; set; }
         public int Received { get; set; }
         public int TotalPointsInPeriod { get; set; }
