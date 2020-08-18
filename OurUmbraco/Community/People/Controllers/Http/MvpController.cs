4ae4c8d9083cb72b2d86e6d5329e1a6ff7ec3b24
hartviglarsen <hartvig@pm.me>
2020-08-18 10:05:47 +0200

    Add endpoint for mvps

---
+++ OurUmbraco/Community/People/Controllers/Http/MvpController.cs
@@ -0,0 +1,23 @@
+﻿using OurUmbraco.Community.People.Models;
+using System.Collections.Generic;
+using Umbraco.Web.WebApi;
+
+namespace OurUmbraco.Community.People.Controllers.Http
+{
+    public class MvpController : UmbracoApiController
+    {
+        private readonly PeopleService _peopleSerivce;
+
+        public MvpController()
+        {
+            _peopleSerivce = new PeopleService();
+        }
+
+        public List<MvpsPerYear> GetAll()
+        {
+            var mvps = _peopleSerivce.GetMvps();
+
+            return mvps;
+        }
+    }
+}
