cd7645216b9b1e56293b5db65a7ecbb324537925
Morten Hartvig <hartvig@pm.me>
2020-09-14 12:49:51 +0200

    Make endpoint for Tweets

---
+++ OurUmbraco/Community/Twitter/Controllers/Http/TwitterController.cs
@@ -0,0 +1,34 @@
+﻿using OurUmbraco.Community.Twitter.Models;
+using System.Linq;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace OurUmbraco.Community.Twitter.Controllers.Http
+{
+    public class TwitterController : UmbracoApiController
+    {
+        private readonly TwitterService _twitterService;
+
+        public TwitterController()
+        {
+            _twitterService = new TwitterService();
+        }
+
+        public IHttpActionResult GetFilteredTweets()
+        {
+            var result = _twitterService.GetTweets(30, false);
+
+            var tweets = result.Tweets.Select(x => new TweetDto
+            {
+                Id = x.Id,
+                ScreenName = x.Author.ScreenName,
+                ProfileImageUrl = x.Author.ProfileImageUrl.Replace("http://", "https://").Replace("_normal.", "."),
+                Text = x.Text,
+                CreatedDate = x.CreatedDate
+            });
+
+
+            return Ok(tweets);
+        }
+    }
+}
