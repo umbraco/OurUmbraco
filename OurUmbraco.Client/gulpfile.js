efabbdc15b37f60237bb7b01b75a1e571fa0967c
Lynne Finnigan <lynnewritescode@gmail.com>
2020-10-17 20:26:36 +0100

    refactor gulp file

--- OurUmbraco.Client/gulpfile.js
+++ OurUmbraco.Client/gulpfile.js
@@ -1,106 +1,94 @@
-var gulp = require('gulp');
+'use strict';
 
-var settings = {
-    build: './build',
-    source: 'src',
-    umbraco: './../OurUmbraco.Site'
+const { task, src, dest, watch, series, parallel } = require('gulp');
+
+const settings = {
+  build: './build',
+  source: 'src',
+  umbraco: './../OurUmbraco.Site',
 };
 
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+const gutil = require('gulp-util');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const imageMin = require('gulp-imagemin');
+const svgmin = require('gulp-svgmin');
+
 // Lint js files
-gulp.task('lint', function () {
-    'use strict';
-
-    var jshint = require('gulp-jshint'),
-        stylish = require('jshint-stylish');
-
-    return gulp.src(settings.source + '/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
-});
+function lint() {
+  return src(settings.source + '/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+}
 
 // Minify js files
-gulp.task('js', function () {
-    'use strict';
-
-    var gutil = require('gulp-util'),
-        uglify = require('gulp-uglify'),
-        concat = require('gulp-concat');
-
-    return gulp.src([settings.source + '/js/vendor/**/*.js', settings.source + '/js/*.js'])
-
-        // Mangle is set to false by default.
-        // By disabling mangle, Uglify won't rename
-        // variables, functions etc.
-        // This means that AngularJS apps will be
-        // able to be minified without breaking.
-        // If you aren't using angular, I recommend
-        // enabling mangle, to obtain better compression.
-        .pipe(uglify({ mangle: false }))
-
-        .pipe(concat('app.min.js'))
-        .on('error', gutil.log)
-        .pipe(gulp.dest(settings.build + '/assets/js'))
-        .pipe(gulp.dest(settings.umbraco + '/assets/js'));
-});
-
-[settings.source + '/css/**/*.css', settings.source + '/scss/style.scss'];
+function js() {
+  return (
+    src([settings.source + '/js/vendor/**/*.js', settings.source + '/js/*.js'])
+      // Mangle is set to false by default.
+      // By disabling mangle, Uglify won't rename
+      // variables, functions etc.
+      // This means that AngularJS apps will be
+      // able to be minified without breaking.
+      // If you aren't using angular, I recommend
+      // enabling mangle, to obtain better compression.
+      .pipe(uglify({ mangle: false }))
+      .pipe(concat('app.min.js'))
+      .on('error', gutil.log)
+      .pipe(dest(settings.build + '/assets/js'))
+      .pipe(dest(settings.umbraco + '/assets/js'))
+  );
+}
 
 // Compile vendor css and scss
-gulp.task('css', function () {
-    var gutil = require('gulp-util'),
-        sass = require('gulp-sass'),
-        autoprefixer = require('gulp-autoprefixer'),
-        cleanCss = require('gulp-clean-css'),
-        rename = require('gulp-rename');
-
-    return gulp.src(settings.source + '/scss/*.scss')
-        .pipe(sass())
-        .on('error', gutil.log)
-        .pipe(autoprefixer('last 1 version', 'ie 9', 'ios 7'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(cleanCss({ level: 2 }))
-        .pipe(gulp.dest(settings.build + '/assets/css'))
-        .pipe(gulp.dest(settings.umbraco + '/assets/css'));
-});
+function css() {
+  return src(settings.source + '/scss/*.scss')
+    .pipe(sass())
+    .on('error', gutil.log)
+    .pipe(autoprefixer('last 1 version', 'ie 9', 'ios 7'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cleanCss({ level: 2 }))
+    .pipe(dest(settings.build + '/assets/css'))
+    .pipe(dest(settings.umbraco + '/assets/css'));
+}
 
 // Optimize images
-gulp.task('images', function () {
-    var gutil = require('gulp-util'),
-        imageMin = require('gulp-imagemin');
-
-    return gulp.src([settings.source + '/images/*.png', settings.source + '/images/*.jpg', settings.source + '/images/*.gif'])
-        .pipe(imageMin())
-        .on('error', gutil.log)
-        .pipe(gulp.dest(settings.build + '/assets/images'))
-        .pipe(gulp.dest(settings.umbraco + '/assets/images'));
-});
+function images() {
+  return src([settings.source + '/images/*.png', settings.source + '/images/*.jpg', settings.source + '/images/*.gif'])
+    .pipe(imageMin())
+    .on('error', gutil.log)
+    .pipe(dest(settings.build + '/assets/images'))
+    .pipe(dest(settings.umbraco + '/assets/images'));
+}
 
 // SVG images
-gulp.task('svg', function () {
-    'use strict';
+function svg() {
+  return src(settings.source + '/images/*.svg')
+    .pipe(svgmin())
+    .on('error', gutil.log)
+    .pipe(dest(settings.build + '/assets/images'))
+    .pipe(dest(settings.umbraco + '/assets/images'));
+}
 
-    var gutil = require('gulp-util'),
-        svgmin = require('gulp-svgmin');
+function watchFiles() {
+  watch([settings.source + '/js/vendor/**/*.js', settings.source + '/js/*.js'], parallel(lint, js));
 
-    return gulp.src(settings.source + '/images/*.svg')
-        .pipe(svgmin())
-        .on('error', gutil.log)
-        .pipe(gulp.dest(settings.build + '/assets/images'))
-        .pipe(gulp.dest(settings.umbraco + '/assets/images'));
-});
+  watch([settings.source + '/scss/**/*.scss', settings.source + '/css/**/*.css'], parallel(css));
+
+  watch(settings.source + '/images/**', parallel(images, svg));
+}
 
 // Build task to used during normal development where you don't need the images to be updated.
-gulp.task('dev', gulp.parallel(['lint', 'js', 'css']));
+exports.dev = parallel(lint, js, css);
 
 // The same as above, but also minifying all the image files.
-gulp.task('build', gulp.parallel(['lint', 'js', 'css', 'images', 'svg']));
+exports.build = series(parallel(lint, js, css, images, svg));
 
 // Default task and watch files
-gulp.task('default', gulp.series('build', function () {
-    'use strict';
-
-
-    gulp.watch([settings.source + '/js/vendor/**/*.js', settings.source + '/js/*.js'], ['lint', 'js']);
-    gulp.watch([settings.source + '/scss/**/*.scss', settings.source + '/css/**/*.css'], ['css']);
-    gulp.watch(settings.source + '/images/**', ['images', 'svg']);
-}));
+exports.default = series(parallel(lint, js, css, images, svg), watchFiles);
