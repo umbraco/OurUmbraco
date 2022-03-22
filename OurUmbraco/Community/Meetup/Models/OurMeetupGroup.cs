a232c376ceae31f3b150f645168cffc8c41f68b9
Sebastiaan Janssen <sebastiaan@umbraco.com>
2022-03-22 15:12:53 +0100

    Whoops notation was a bit too modern

--- OurUmbraco/Community/Meetup/Models/OurMeetupGroup.cs
+++ OurUmbraco/Community/Meetup/Models/OurMeetupGroup.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using Skybrud.Social.Meetup.Models.GraphQl.Groups;
 
-namespace OurUmbraco.Community.Meetup.Models;
-
-public class OurMeetupGroup
+namespace OurUmbraco.Community.Meetup.Models
 {
-    public MeetupGroup Group { get; set; }
-    public List<Skybrud.Social.Meetup.Models.GraphQl.Events.MeetupEvent> Events { get; set; }
+    public class OurMeetupGroup
+    {
+        public MeetupGroup Group { get; set; }
+        public List<Skybrud.Social.Meetup.Models.GraphQl.Events.MeetupEvent> Events { get; set; }
+    }
 }
