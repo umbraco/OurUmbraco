233c2450bc15562cbf3edba67c91b1a575c4a6f2
Sebastiaan Janssen <sebastiaan@umbraco.com>
2025-02-05 17:24:00 +0100

    Initial work to create topic on new forum with "AJAX"

---
+++ OurUmbraco/Forum/Controllers/DiscourseController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using OurUmbraco.Forum.Models;
+using OurUmbraco.Forum.Services;
+using System.Web.Http;
+using Umbraco.Core;
+using Umbraco.Web.WebApi;
+
+namespace OurUmbraco.Forum.Controllers
+{
+    public class DiscourseController : UmbracoApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post(TopicModel topicModel)
+        {
+            var topicService = new TopicService(ApplicationContext.DatabaseContext);
+            var topic = topicService.GetById(topicModel.TopicId);
+            var body = topic.Body.Replace(": /media/upload/", ": https://our.umbraco.com/media/upload/");
+            body = body + "\n<hr>\n<small>This is a companion discussion topic for the original entry at <a href=\"https://cultiv.nl/blog/released-search-engine-sitemap-package\">https://cultiv.nl/blog/released-search-engine-sitemap-package</a></small>";
+            var discourseCreateTopic = new DiscourseCreateTopic
+            {
+                Title = topic.Title,
+                Raw = body,
+                Category = 5,
+                CreatedAt = topic.Created,
+                EmbedUrl = $"https://our.umbraco.com/forum/using-umbraco/{topic.Id}-{topic.Title.ToUrlSegment()}",
+                ExternalId = topic.Id
+            };
+
+            var x = discourseCreateTopic;
+            return Ok(x);
+        }
+
+        public class TopicModel
+        {
+            public int TopicId { get; set; }
+        }
+
+    }
+}
---
+++ OurUmbraco/Forum/Models/DiscourseCreateTopic.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace OurUmbraco.Forum.Models
+{
+    public class DiscourseCreateTopic
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("raw")]
+        public string Raw { get; set; }
+
+        [JsonProperty("category")]
+        public int Category { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("embed_url")]
+        public string EmbedUrl { get; set; }
+
+        [JsonProperty("external_id")]
+        public int ExternalId { get; set; }
+    }
+}
