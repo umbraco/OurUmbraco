e2e4467b6bf86a9d876a7b700aaf6a283211608b
Sebastiaan Janssen <sebastiaan@umbraco.com>
2025-04-15 11:11:54 +0200

    error handling for meetups feed

--- OurUmbraco/Community/Controllers/MeetupsController.cs
+++ OurUmbraco/Community/Controllers/MeetupsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OurUmbraco.Forum.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
@@ -6,6 +7,7 @@ using System.Text.Json.Serialization;
 using System.Web.Mvc;
 using Umbraco.Core.Cache;
 using Umbraco.Web;
+using Umbraco.Web.Models.ContentEditing;
 using Umbraco.Web.Mvc;
 
 namespace OurUmbraco.Community.Controllers
@@ -19,24 +21,34 @@ namespace OurUmbraco.Community.Controllers
             return PartialView("~/Views/Partials/Home/Meetups.cshtml", events);
         }
 
-        public static List<EventFeedModel> GetMeetups()
+        public List<EventFeedModel> GetMeetups()
         {
             return UmbracoContext.Current.Application.ApplicationCache.RuntimeCache.GetCacheItem<List<EventFeedModel>>("EventFeed",
                 () =>
                 {
                     var eventFeed = new List<EventFeedModel>();
-                    using (var client = new HttpClient())
+                    try
                     {
-                        using (var result = client.GetAsync("https://umbracalendar.com/api/EventFeed/").Result)
+                        using (var client = new HttpClient())
                         {
-                            if (result.IsSuccessStatusCode == false)
+                            var feedUrl = "https://umbracalendar.com/api/EventFeed/";
+                            using (var result = client.GetAsync(feedUrl).Result)
                             {
-                                // log?
-                            }
+                                if (result.IsSuccessStatusCode == false)
+                                {
+                                    Logger.Warn(typeof(MeetupsController), $"Failed to get {feedUrl}, result: {result.StatusCode} || {result.ReasonPhrase} ");
 
-                            var json = result.Content.ReadAsStringAsync().Result;
-                            eventFeed = JsonSerializer.Deserialize<List<EventFeedModel>>(json);
+                                }
+
+                                var json = result.Content.ReadAsStringAsync().Result;
+                                eventFeed = JsonSerializer.Deserialize<List<EventFeedModel>>(json);
+                            }
                         }
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Error(typeof(MeetupsController), "Fetching event feed not successful" + ex.Message, ex);
+                        Logger.Error(typeof(MeetupsController), "InnerException" + ex.InnerException.Message, ex.InnerException);
                     }
 
                     return eventFeed;
