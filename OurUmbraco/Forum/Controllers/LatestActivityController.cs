92be0a134b83f8781b4fa9a9f1ae0c9d7ca5b3db
Sebastiaan Janssen <sebastiaan@umbraco.com>
2025-09-04 11:05:07 +0200

    Fix edge case error when topics list is temporarily empty

--- OurUmbraco/Forum/Controllers/LatestActivityController.cs
+++ OurUmbraco/Forum/Controllers/LatestActivityController.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
-using OurUmbraco.Community.People;
 using OurUmbraco.Forum.Services;
 using Umbraco.Web.Mvc;
 
@@ -11,9 +11,14 @@ namespace OurUmbraco.Forum.Controllers
         public ActionResult LatestActivity(int numberOfTopics = 10)
         {
             var discourseService = new DiscourseService();
-            var discourseTopics = discourseService.GetLatestTopics("questions", 5).Take(numberOfTopics).ToList();
+            var discourseTopics = discourseService.GetLatestTopics("questions", 5);
+            var topics = new List<Models.DiscourseTopic>();
+            if(discourseTopics != null)
+            {
+                topics = discourseTopics.Take(numberOfTopics).ToList();
+            }
 
-            return PartialView("~/Views/Partials/Home/LatestForumActivity.cshtml", discourseTopics);
+            return PartialView("~/Views/Partials/Home/LatestForumActivity.cshtml", topics);
         }
     }
 }
